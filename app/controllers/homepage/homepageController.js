44cb66e028f0a61c14bf5fb792e71f354b9b9971
NACER GARGA <ngarga@ebay.com>
2015-12-14 11:26:59 -0800

    implementating gulp for styles and scripts

--- app/controllers/homepage/homepageController.js
+++ app/controllers/homepage/homepageController.js
@@ -9,7 +9,11 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
   var articles = [new Article(), new Article()];
     res.render('homepage/views/hbs/homepage_es_MX', {
-      title: 'Generator-Express MVC',
-      articles: articles
+      	title: 'Generator-Express MVC',
+      	articles: articles,
+		env: 'public',
+		locale: 'en_ZA',
+		site: 'Gumtree',
+		pagename: 'homepage'
     });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,101 @@
+/ ////////////////////////////////////////////////
+//
+// EDIT CONFIG OBJECT BELOW !!!
+// 
+// jsFiles => list of javascript files (in order) to concatenate
+// buildFilesFoldersRemove => list of files to remove when running final build
+// // //////////////////////////////////////////////
+
+var jsFiles = require('./app/config/components_en_ZA.json');
+var jsNeededForPage = {};
+
+
+//converting to actual path
+for (var key in jsFiles){
+  for(var i=0; i<jsFiles[key].length; i++){
+    jsNeededForPage[key] = [];
+    jsNeededForPage[key][i] = './views/components/' + jsFiles[key][i] + '/js/*.js';
+  }
+}
+
+/////////////////////////////////////////////////
+// Required tasks
+// gulp build
+// bulp build:serve
+// // /////////////////////////////////////////////
+
 var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload,
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  del = require('del');
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
-  sass = require('gulp-ruby-sass');
+  sassO = require('gulp-ruby-sass');
 
-gulp.task('sass', function () {
-  return sass('./public/css/**/*.scss')
+
+// ////////////////////////////////////////////////
+// Log Errors
+// // /////////////////////////////////////////////
+
+function errorlog(err){
+  console.error(err.message);
+  this.emit('end');
+}
+
+
+// ////////////////////////////////////////////////
+// Scripts Tasks
+// ///////////////////////////////////////////////
+gulp.task('scripts', function() {
+  for (var key in jsFiles){
+    gulp.src(jsNeededForPage[key])
+    .pipe(concat('temp.js'))
+        .pipe(uglify())
+        .on('error', errorlog)  
+        .pipe(rename(key + '.min.js'))  // <- HERE
+        .pipe(gulp.dest('./.build/js/'))
+
+        .pipe(reload({stream:true}));
+  }
+});
+
+
+// ////////////////////////////////////////////////
+// Styles Tasks
+// ///////////////////////////////////////////////
+
+gulp.task('styles', function() {
+  gulp.src('./public/styles/**/**/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'compressed'}))
+      .on('error', errorlog)
+      .pipe(autoprefixer({
+              browsers: ['last 3 versions'],
+              cascade: false
+          })) 
+    .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('./public/css'))
     .pipe(livereload());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./public/css/*.scss', ['sass']);
+
+// ////////////////////////////////////////////////
+// Watch Tasks
+// // /////////////////////////////////////////////
+
+gulp.task ('watch', function(){
+  gulp.watch('./public/styles/**/**/*.scss', ['styles']);
+  gulp.watch('./public/js', ['scripts']);
+  //gulp.watch('app/**/*.html', ['html']);
 });
+
 
 gulp.task('develop', function () {
   livereload.listen();
@@ -31,10 +114,10 @@ gulp.task('develop', function () {
   });
 });
 
-gulp.task('build', ['sass']);
+gulp.task('build', ['styles']);
 
-gulp.task('default', [
-  'sass',
-  'develop',
-  'watch'
-]);
+gulp.task('default', ['scripts', 'styles', 'develop', 'watch']);
+
+
+
+
