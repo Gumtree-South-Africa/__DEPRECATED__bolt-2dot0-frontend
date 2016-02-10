264daef772727e24c6515530f1106fa57306d839
prsheth <prsheth@ebay.com>
2016-02-10 14:55:47 -0800

    adding new command line param for site locales

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ var checksite = require('./server/middlewares/check-site');
 
 var controllers = glob.sync(process.cwd() + '/app/controllers/**/*.js');
 var config = require('./server/config/sites.json');
+// default list of all locales, if new locales are added, add it in this list
+var allLocales = "es_MX,es_AR,es_US,en_ZA,en_IE,pl_PL,en_SG";
+// if SITES param is passed as input param, load only those countries
+var siteLocales = process.env.SITES || allLocales;
 
 /*
  * Create Main App
@@ -36,58 +40,59 @@ i18next
     }
   });
 
-
 /*
  * Create Site Apps
  */
+ // validate if process.env.site is a valid string with valid locales
  Object.keys(config.sites).forEach(function(siteKey) {
+
     var siteObj = config.sites[siteKey];
-    
-    (function(siteObj) {
-      var builderObj = new expressbuilder(siteObj.locale);
-      // var siteApp = new expressbuilder(siteObj.locale).getApp();
-      var siteApp = builderObj.getApp();
+    if (siteLocales.indexOf(siteObj.locale) > -1) {
+      (function(siteObj) {
+        var builderObj = new expressbuilder(siteObj.locale);
+        // var siteApp = new expressbuilder(siteObj.locale).getApp();
+        var siteApp = builderObj.getApp();
 
-      // send site information along
-      siteApp.config = {};
-      siteApp.config.name = siteObj.name;
-      siteApp.config.locale = siteObj.locale;
-      siteApp.config.country = siteObj.country;
-      siteApp.config.hostname = siteObj.hostname;
-      siteApp.config.hostnameRegex = '[\.-\w]*' + siteObj.hostname + '[\.-\w-]*';
+        // send site information along
+        siteApp.config = {};
+        siteApp.config.name = siteObj.name;
+        siteApp.config.locale = siteObj.locale;
+        siteApp.config.country = siteObj.country;
+        siteApp.config.hostname = siteObj.hostname;
+        siteApp.config.hostnameRegex = '[\.-\w]*' + siteObj.hostname + '[\.-\w-]*';
 
-      // set req.lng to defined lng in vhost
-      siteApp.use(function(req, res, next) {
-        //var i18nObj = req.t;
-        // builderObj.setI18nObj(i18nObj);
-        req.lng = siteApp.config.locale;
-        i18next.changeLanguage(siteApp.config.locale);
-        // req.i18n.changeLanguage(siteApp.config.locale);
-        next();
-      });
+        // set req.lng to defined lng in vhost
+        siteApp.use(function(req, res, next) {
+          //var i18nObj = req.t;
+          // builderObj.setI18nObj(i18nObj);
+          req.lng = siteApp.config.locale;
+          i18next.changeLanguage(siteApp.config.locale);
+          // req.i18n.changeLanguage(siteApp.config.locale);
+          next();
+        });
 
-      // use the middleware to do the magic
-      // create a fixed t function for req.lng
-      // no clones needed as they just would do the same (sharing all but lng)
-      siteApp.use(middleware.handle(i18next));
+        // use the middleware to do the magic
+        // create a fixed t function for req.lng
+        // no clones needed as they just would do the same (sharing all but lng)
+        siteApp.use(middleware.handle(i18next));
 
-      // Template hbs caching.
-      // See: https://github.com/ericf/express-handlebars#template-caching
-      // Enables view template compilation caching and is enabled in production by default.
-      if (process.env.NODE_ENV) {
-        siteApp.enable('view cache');
-      }
+        // Template hbs caching.
+        // See: https://github.com/ericf/express-handlebars#template-caching
+        // Enables view template compilation caching and is enabled in production by default.
+        if (process.env.NODE_ENV) {
+          siteApp.enable('view cache');
+        }
 
-      // register bolt site checking middleware
-      siteApp.use(checksite(siteApp));
-      
-      // Setup Vhost per supported site
-      app.use(vhost(new RegExp(siteApp.config.hostnameRegex), siteApp));
-    })(siteObj);
+        // register bolt site checking middleware
+        siteApp.use(checksite(siteApp));
 
-    siteCount = siteCount + 1;
+        // Setup Vhost per supported site
+        app.use(vhost(new RegExp(siteApp.config.hostnameRegex), siteApp));
+      })(siteObj);
+      siteCount = siteCount + 1;
+    }
  });
- 
+
 //Setup controllers
 controllers.forEach(function (controller) {
     require(controller)(app);
@@ -100,14 +105,14 @@ function clearRequireCache(mod) {
 	console.log(mod);
   	Object.keys(require.cache).forEach(function(key) {
   		if (key.indexOf(mod) > -1) {
-//  			console.log("found");  
+//  			console.log("found");
 //  			console.log(key);
   			delete require.cache[key];
 		}
-  	});  
+  	});
 }
 
 function requireUncached(module){
 	delete require.cache[require.resolve(module)];
-    return require(module); 
+    return require(module);
 }
