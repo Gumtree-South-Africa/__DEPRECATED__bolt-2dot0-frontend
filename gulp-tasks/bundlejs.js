7f9dce7d74ce914a8075b2bd3b1e26698399be4c
NACER GARGA <ngarga@ebay.com>
2016-02-19 11:45:13 -0800

    jsmin and aggragation

--- gulp-tasks/bundlejs.js
+++ gulp-tasks/bundlejs.js
@@ -1,64 +1,14 @@
 'use strict';
 
-// ////////////////////////////////////////////////
-// Compass Tasks
-// ///////////////////////////////////////////////
-// module.exports = function watch(gulp, plugins) {
-//
-// var es = require('event-stream');
-// var gulp = require('gulp');
-//
-// var bundles = [
-//   {
-//     src: 'js/my-component/*.js',
-//     bundleName: 'my-component.js'
-//   },
-//   {
-//     src: 'js/my-other-component/*.js',
-//     bundleName: 'my-other-component.js'
-//   }
-// ];
-//
-// gulp.task('bundlejs', function () {
-//   return function(){
-//     console.log('hhhh');
-//     es.merge(bundles.map(function (obj) {
-//     console.log('jjjj');
-//     console.log(obj);
-//     // return gulp.src(obj.src)
-//     //   .pipe(concat(obj.bundleName))
-//     //   .pipe(gulp.dest('js/_bundles'));
-//   }));
-// }});
-//
-// }
-
-
-'use strict';
-
 //////////////////////////////////////////////////
-//Clean Task
+// javascript Aggragation and Minification Task
 //// /////////////////////////////////////////////
 
 var es = require('event-stream'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify');
 
-var bundles = [
-  {
-    dest: process.cwd() + '/public/testJ',
-    src: [process.cwd() + '/public/js/common/*.js',
-          process.cwd() + '/public/js/libraries/jquery-2.0.0.js'
-         ],
-    bundleName: 'my-component.min.js'
-  },
-  {
-    dest: process.cwd() + '/public/testJ',
-    src: process.cwd() + '/public/js/libraries/handlebars/*.js',
-    bundleName: 'my-other-component.min.js'
-  }
-];
-
+var bundles = require(process.cwd() + '/app/config/ruby/jsmin');
 
 module.exports = function watch(gulp, plugins) {
 	return function(){
--- gulp-tasks/compass.js
+++ gulp-tasks/compass.js
@@ -5,65 +5,19 @@
 // ///////////////////////////////////////////////
 module.exports = function watch(gulp, plugins) {
 	return function(){
-		var folderList = [];
 		gulp.task('compass', function(){
-
-      var emitter = walkdir(process.cwd() + '/app/config/ruby', {no_recurse: true}, function(dir, stat, depth){
-        var base = path.basename(dir);
-        folderList.push(base);
-      });
-
-			emitter.on('end', function(){
-				//(function(){
-				var config = require(process.cwd() + '/app/config/ruby/compassConfig');
-				console.log(config);
-				//for (var i=0; i<folderList.length; i++){
-				  gulp.src('./app/styles/**/**/*.scss')
-							.pipe(plumber({
-								errorHandler: function (error) {
-									console.log(error.message);
-									this.emit('end');
-							}}))
-				      .pipe(compass({
-				        project: process.cwd(),
-								lineNumbers: config.lineNumbers,
-								css: config.cssPath,
-								sass: config.sassPath,
-								require: ['susy']
-				      }))
-				      .pipe(gulp.dest('./public/css'))
-				      .pipe(notify({
-			            title: 'Compass',
-			            message: 'Style Compilation done. Wonderful!'
-			        }));
-				//	}
-				//})
-
-			//})();
-			})
+		  gulp.src('./app/styles/**/**/*.scss')
+		      .pipe(compass({
+		        config_file: process.cwd() + '/app/config/ruby/config.rb',
+		        css: 'public/css',
+		        sass: './app/styles',
+		        require: ['susy']
+		      }))
+		      .pipe(gulp.dest('./public/css'))
+		      .pipe(notify({
+	            title: 'Compass',
+	            message: 'Style Compilation done. Wonderful!'
+	        }));
 		})
 	}
 }
-
-//
-// (function() {
-// 		var index = 0;
-// 		folderList.push('default_country');
-// 		function perLocale(){
-// 			var listItem = folderList[index];
-// 			var config = require(process.cwd() + "/app/config/gulpIcons/gulpsvgconfig.js")(listItem);
-// 					config.dest = "./public/css";
-// 			var files = glob.sync("./public/svgs/"+ listItem +"/*.svg");
-// 			if(index < folderList.length-1){
-// 				index++;
-// 				gulpicon(files, config)(perLocale);
-// 			}
-// 			else{
-// 				notify({
-// 						title: 'Compass',
-// 						message: 'SVGs Tasks done. Please start your app on a Browser'
-// 				})
-// 			}
-// 		}
-// 		perLocale();
-// })();
