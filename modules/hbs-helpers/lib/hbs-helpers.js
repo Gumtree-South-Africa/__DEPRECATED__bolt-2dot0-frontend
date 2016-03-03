d320f58083a3caa5f393e97fe409a1602be7b3ad
aganeshalingam <aganeshalingam@ebay.com>
2016-03-03 08:29:30 -0800

    Adding check for undefined values for hbs helpers

--- modules/hbs-helpers/lib/hbs-helpers.js
+++ modules/hbs-helpers/lib/hbs-helpers.js
@@ -33,6 +33,7 @@ module.exports  =  {
 
         exphbs.handlebars.registerHelper("partial",
             function (name, options) { //console.log( "partial " + name);
+                if (!name) return;
                 // console.log("--------" +  util.inspect(options.fn, {showHidden: false, depth: null}));
                 exphbs.handlebars.registerPartial(name, options.fn);
             }
@@ -40,22 +41,25 @@ module.exports  =  {
 
         exphbs.handlebars.registerHelper("block",
             function (name, options) { //console.log("block++++++++++++++ " + util.inspect(options, {showHidden: false, depth: null}));
-
+                if (!name) return;
                 var partial = loadPartial(name) || options.fn;
                 return new exphbs.handlebars.SafeString(partial(this, { data : options.hash }));
             }
         );
 
         exphbs.handlebars.registerHelper('dynamic', function(partialName, options) {
-            //console.log("dp " + options.explicitPartialContext);
+            if (!partialName) return;
+           // console.log("dp " + options.explicitPartialContext);
             return new exphbs.handlebars.SafeString(partialName + "/views/hbs/" + partialName + "_" + this.locale);
         });
 
         exphbs.handlebars.registerHelper('base', function(partialName, options) {
+            if (!partialName) return;
             return new exphbs.handlebars.SafeString(partialName + "/views/hbs/" + partialName);
         });
 
         exphbs.handlebars.registerHelper('include', function(component, subPartialName, options) {
+            if (!component) return;
             return new exphbs.handlebars.SafeString(component + "/views/hbs/" + subPartialName);
         });
 
@@ -73,7 +77,8 @@ module.exports  =  {
         });
 
 
-        exphbs.handlebars.registerHelper('i18n', function (msg, value) {
+        exphbs.handlebars.registerHelper('i18n', function (msg, value) { //console.log("xxxxxxx -" + msg);
+            if (!msg) return;
             // if there are 3 param values in {{i18n "my.name is %s. i'm %s old. I live in, %s" "anton" "20" "santa cruz"}}
             if (arguments.length == 5) {
                 return new exphbs.handlebars.SafeString( obj.app.locals.i18n.__(msg, arguments[1], arguments[2], arguments[3]));
@@ -96,14 +101,17 @@ module.exports  =  {
         });
 
         exphbs.handlebars.registerHelper('json', function(context) {
+            if (!context) return;
             return new exphbs.handlebars.SafeString(JSON.stringify(context));
         });
 
         exphbs.handlebars.registerHelper('obfuscateUrl', function(value) {
+            if (!value) return;
             return new exphbs.handlebars.SafeString(StringUtils.obfuscate(value));
         });
 
         exphbs.handlebars.registerHelper('digitGrouping', function(number, separator) {
+            if (!number) return;
             number = parseFloat(number);
             separator = util.isUndefined(separator) ? ',' : separator;
             return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1" + separator);
@@ -111,21 +119,25 @@ module.exports  =  {
 
 
         exphbs.handlebars.registerHelper('ifDesktop', function(val, options) {
+            if (!val) return;
             var fnTrue=options.fn, fnFalse=options.inverse;
             return val.isDesktop ? fnTrue() : fnFalse();
         });
 
         exphbs.handlebars.registerHelper('ifMobile', function(val, options) {
+            if (!val) return;
             var fnTrue=options.fn, fnFalse=options.inverse;
             return val.isMobile ? fnTrue() : fnFalse();
         });
 
         exphbs.handlebars.registerHelper('ifTablet', function(val, options) {
+            if (!val) return;
             var fnTrue=options.fn, fnFalse=options.inverse;
             return val.isTablet ? fnTrue() : fnFalse();
         });
 
         exphbs.handlebars.registerHelper('unlessMobile', function(val, options) {
+            if (!val) return;
             // console.log("isTablet xxxxxxxxxxxxxxxxxxxxxxxxxx"  + util.inspect(val.isTablet, {showHidden: false, depth: 1}));
             var fnTrue=options.fn, fnFalse=options.inverse;
             return val.isTablet || val.isDesktop? fnTrue() : fnFalse();
