d60e3b8e0acd267156188e108989bec24e4a0a75
NACER GARGA <ngarga@ebay.com>
2016-03-03 15:12:17 -0800

    improvement of pak task

--- gulp-tasks/pak.js
+++ gulp-tasks/pak.js
@@ -1,22 +1,63 @@
 'use strict';
 
 // ////////////////////////////////////////////////
-// Bundling Tasks
+// PACKAGING Tasks
 // ///////////////////////////////////////////////
 
 module.exports = function watch(gulp, plugins) {
 	return function(){
     var appVersion = require(process.cwd() + "/server/config/production.json").static.server.version;
-    var folderList = [];
 
-    gulp.task('pak', function() {
+		// Distribution Package
+		gulp.task('pak:dist', function(){
+			process.stdout.write('Distribution Package Task is running...\r\n');
+
+			var stream =
+         gulp.src(['./**/*', '!./{target,target/**}', '!./{public,public/**}'], {base: './'})
+          .pipe(plugins.tar('bolt-2dot0-frontend-' + appVersion + '.tar'))
+        	.pipe(plugins.gzip())
+          .pipe(gulp.dest('./target/' + appVersion + '/dist'))
+          .on('end', function(){
+              console.log('Congratulations!!! DIST PACKAGING DONE SUCCESSFULLY');
+          });
+
+					return stream;
+    });
+
+		//Static Package
+    gulp.task('pak:static', function() {
+			process.stdout.write('Static Package Task is running...\r\n');
+
+			var stream =
       gulp.src(process.cwd() + "/" + "public/**/*/")
-        .pipe(tar('bolt-2dot0-frontend-static-'+ appVersion +'.tar'))
-        .pipe(gzip())
+        .pipe(plugins.tar('bolt-2dot0-frontend-static-'+ appVersion +'.tar'))
+        .pipe(plugins.gzip())
         .pipe(gulp.dest('./target/' + appVersion + '/static'))
 				.on('end', function(){
 	          console.log('Congratulations!!! STATIC PACKAGING DONE SUCCESSFULLY');
 	      })
-    })
+
+				return stream;
+    });
+
+		
+		gulp.task('pak', function (callback) {
+			loadingSpinner.start();
+
+			var stream =
+			runSequence(
+				['pak:dist', 'pak:static'],
+				function (error) {
+					if (error) {
+						console.log(error.message);
+					} else {
+						loadingSpinner.stop();
+						console.log('Congratulations!!! PACKAGING DONE SUCCESSFULLY');
+					}
+					callback(error);
+				});
+
+				return stream;
+		 });
   }
 }
