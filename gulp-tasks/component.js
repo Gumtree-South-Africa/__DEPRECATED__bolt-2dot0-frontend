f62cc1f4d999f83c4dcf2bf9416bf603867cc7d2
NACER GARGA <ngarga@ebay.com>
2016-03-04 10:15:29 -0800

    component templates names are now updated to reflect their name

--- gulp-tasks/component.js
+++ gulp-tasks/component.js
@@ -4,35 +4,50 @@
 // Component Tasks : to create components
 // Script to create: gulp component -n [name-of-the-component]
 // ///////////////////////////////////////////////
+var del = require('del');
+
 module.exports = function watch(gulp, plugins) {
   return function (){
-    var componentExist = false,
-    err = {
-      'message': 'There already is a component with the same name'
-    };
+    var componentName,
+        componentExist = false,
+        cwd = process.cwd(),
+        err = {
+          'message': 'There already is a component with the same name'
+        };
 
-	gulp.task('component', function() {
+  	gulp.task('component', function() {
+  	  componentName = argv.n;
+  	  var emitter = walkdir(process.cwd() + '/app/views/components', {no_recurse: true}, function(dir, stat, depth){
+  	    var base = path.basename(dir);
+  	    if(componentName.toString() == base){
+  	      console.log(new Error(err.message));
+  	      componentExist = true;
+  	      this.end();
+  	    }
+  	  });
 
-	  var componentName = argv.n;
-	  var emitter = walkdir(process.cwd() + '/app/views/components', {no_recurse: true}, function(dir, stat, depth){
-	    var base = path.basename(dir);
-	    if(componentName.toString() == base){
-	      console.log(new Error(err.message));
-	      componentExist = true;
-	      this.end();
-	    }
-	  });
+  	  emitter.on('end', function(){
+  	    if(!componentExist){
+    	      gulp.src(cwd + '/app/views/components/__default/**/*',
+    	        {
+    	           base:cwd + '/app/views/components/__default'
+    	        })
+    	       .pipe(gulp.dest(cwd + '/app/views/components/'+ componentName))
+             .on('end', function(){
+               gulp.src(cwd + '/app/views/components/'+ componentName +'/views/tmp/**/*.hbs')
+                 .pipe(plugins.rename(function (path) {
+                   path.basename = path.basename.replace(/default/ig, componentName);
+                 }))
+                 .pipe(gulp.dest(cwd + '/app/views/components/'+ componentName +'/views/hbs'))
+                 .on('end', function(){
+                   del([cwd + '/app/views/components/'+ componentName +'/views/tmp']);
+                 });
+             })
 
-	  emitter.on('end', function(){
-	    if(!componentExist){
-	      return gulp.src(process.cwd() + '/app/views/components/__default/**/*',
-	        {
-	           base: process.cwd() + '/app/views/components/__default'
-	        })
-	       .pipe(gulp.dest(process.cwd() + '/app/views/components/'+ componentName))
-	    }
-	  });
+    	  }
+  	  });
 
-	});
+      return emitter;
+  	});
   };
 };
