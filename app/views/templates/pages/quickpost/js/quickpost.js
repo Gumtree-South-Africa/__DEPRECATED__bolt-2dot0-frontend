44a051eece87ecfa86cb784b958905cd0f38dbc4
Robles <uroblesmellin@ebay.com>
2016-04-12 15:25:21 -0700

    Code refactoring

--- app/views/templates/pages/quickpost/js/quickpost.js
+++ app/views/templates/pages/quickpost/js/quickpost.js
@@ -1,32 +1,140 @@
 var BOLT = BOLT || {};
 
-BOLT.Homepage = (function () {
-	return {
-		setUp : function () {
-			this.getTemplateData();
-		},
+/**
+ * @description A singleton class that implements the handling of the Quickpost page
+ * @namespace BOLT
+ * @class QuickPostPage
+ */
+BOLT.QuickPostPage = (function() {
+    var GMapObj = BOLT.MapLatLong;
+    var catMobileSelObj = null;
 
-		getTemplateData : function () {
-			var html = "";
-			var dataObj = {
-				name : "User Precompiled",
-				title : "Success!"
-			};
+   /**
+    * @method openMap
+    * @description Opens a Gmap region     
+    * @private
+    */
+    function openMap() {
+        if (!openMap.opened) {
+            $("#map_canvas_wrapper").slideDown("slow");
+        } else {
+            $("#map_canvas_wrapper").slideUp("slow", function() {
+                // var latlongData = GMapObj.getLatLong();
+                setMapCoords(GMapObj.getLatLong());
+            });
+        }
+        openMap.opened = !openMap.opened;
+    }
+    openMap.opened = false;
 
-			var template = Handlebars.template;
-			var homepageTemplate = Handlebars.templates['a'];
-			if (typeof homepageTemplate === "object") {
-				homepageTemplate = template(homepageTemplate);
-			}
+   /**
+    * @method setMapCoords
+    * @description Sets the Gmap coordinates
+    * @param {Object} latlongData     
+    * @private
+    */
+    function setMapCoords(latlongData) {
+    	$("#latitude").val(latlongData.lat);
+    	$("#longitude").val(latlongData.long);
+    	$("#address").val(latlongData.address);
+    	$("#location").val(latlongData.address);
+	}
 
-			if (typeof homepageTemplate === "function") {
-				html = homepageTemplate(dataObj);
-			}
+   /**
+    * @method mobileCategorySelector
+    * @description Selects a Category from the Mobile Selector/Menu     
+    * @private
+    */
+    function mobileCategorySelector() {
+    	if (typeof Bolt === "undefined" || typeof BOLT === "undefined") {
+    		return false;
+    	}
 
-			$(".precompiled").html(html);
+        var PAC = BOLT.POSTAD.MobileItemSelector || undefined;
+        var pageMode;
+        var mobileCatConfig = {
+            title: Bolt._postFormMsgs.selectCategoryLabel || "",
+            searchPlaceholder: Bolt._postFormMsgs.categorySearchPlaceholder || "",
+            highlightItemOnNewPanel: false,
+            selectors: {
+                menuContainer: "#cat-list",
+                mobileContainer: "#mobileCats",
+                basePanel: ".base-panel",
+                itemListPanel: ".item-list"
+            }
+        };
 
-			return html;
-		}
-	};
+        if (typeof PAC !== "undefined" && $("#mobileCats").length > 0) {
+            if (!catMobileSelObj) {
+                catMobileSelObj = new PAC();
+            }
+        }
 
-})();
+        if (catMobileSelObj.hasRendered()) {
+            $("#catSelector").show();
+            $("#mobileCats").show();
+            $("#cat-list").trigger("MobileListNodesReady");
+            return;
+        }
+
+        pageMode = {
+            idEdit: false,
+            isPreview: false,
+            isSimilarAd: false
+        };
+
+        if (catMobileSelObj) {
+            catMobileSelObj.init(mobileCatConfig, pageMode);
+        }
+    }
+
+
+    // Public methods
+    return {
+
+        /**
+         * @method init
+         * @description Handles the initial state
+         * @public
+         */
+        init: function() {
+        	mobileCategorySelector();
+        	this.syncUI();
+        },
+
+        /**
+         * @method syncUI
+         * @description Handles the initial EVENT manipulations for this class
+         * @public
+         */
+        syncUI : function () {
+            $("#maps-link").click(openMap);
+
+            $(document).on("applyLocation", function(e, latlongData) {
+            	setMapCoords(latlongData);
+            });
+
+            // Listener to update the category Id when the user selects a category.
+            $(document).on("MobileLeafReached", function(e, obj) {
+                if (catMobileSelObj === obj.currentInstance) {
+                    window.setTimeout(function() {
+                        $("#mobileCats .base-panel-container > ul > li").removeClass("mm-hidden");
+
+                        // Set the Category name
+                        $("#mobileCats .base-panel .initial-label").text(obj.label);
+                    }, 100);
+
+                    // Set the Category Id in a hidden var.
+                    $("input[name=category]").val(obj.id);
+                }
+            });
+        }
+
+    }; // return
+
+})(); // Ends singleton
+
+// Start the page main JS.
+$(document).ready(function() {
+	BOLT.QuickPostPage.init();
+});
