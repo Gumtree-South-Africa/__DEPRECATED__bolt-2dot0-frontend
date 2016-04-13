7cd8a609f59a1433e3981cb8276ed6ba157c2a31
Robles <uroblesmellin@ebay.com>
2016-04-12 17:06:26 -0700

    Fixed BOLT-20988

--- public/js/common/header/searchbar.js
+++ public/js/common/header/searchbar.js
@@ -602,14 +602,14 @@ $(document).ready(function() {
      * ========================================================================================================
      */
     var geoFeatureSupported = window.navigator.geolocation && "getCurrentPosition" in window.navigator.geolocation;
-
+    var criteoOrigZIndex;
     /**
      * This geolocator class will attach a handler on the page to locate the user
      * @constructor
      */
-    function GeoLocator(config){
-
+    function GeoLocator(config) {
         var scope = this;
+
         function freezeBody(toFraze)
         {
             if(toFraze)
@@ -660,6 +660,7 @@ $(document).ready(function() {
                         scope.close();
                     })
                     .on("click", "div", function(event){
+                        processPostOverlayClose();
                         event.stopPropagation();
                     })
                     .on("click", "a", function(){
@@ -727,6 +728,7 @@ $(document).ready(function() {
 
 
         $searchbar.on("click", ".geo-locator", function() {
+            processPreOverlayOpen()
             geoLocator.select(saveSelection);
         });
 
@@ -737,6 +739,20 @@ $(document).ready(function() {
             Bolt.Cookie.setHardCookie("promptGeo", true);
         }
 
+    }
+
+
+    // Method that has the extra code to be executed BEFORE the overlay gets opened
+    function processPreOverlayOpen() {
+        // BOLT-20988
+        criteoOrigZIndex = $("div.criteo_header").css("z-index");
+        $("div.criteo_header").css("z-index", 0);
+
+    }
+
+    // Method that has the extra code to be executed AFTER the overlay gets opened
+    function processPostOverlayClose() {
+        $("div.criteo_header").css("z-index", criteoOrigZIndex)
     }
     /*
      * ========================================================================================================
