b434a2ac65cfb4d820836201392577740bd51d9a
Dennis ONeill <denoneill@ebay.com>
2016-06-13 16:29:59 -0700

    added more lint rules, expanded lint diretory for isotope client script, added intellij code style settings xml

--- gulp-tasks/eslint.js
+++ gulp-tasks/eslint.js
@@ -8,11 +8,12 @@ const gulpEslint = require("gulp-eslint");
 module.exports = function watch(gulp, plugins) {
 	return function () {
 
-		gulp.task('eslint', function() {
+		gulp.task('eslint', function () {
 			var stream =
 				gulp.src([
-						'app/controllers/page/isotopePrototype/*.js'
-					])
+					'app/controllers/page/isotopePrototype/*.js',
+					'app/views/components/tileGrid/js/*.js'
+				])
 					.pipe(gulpEslint())
 					.pipe(gulpEslint.format())
 					.pipe(gulpEslint.failAfterError());
