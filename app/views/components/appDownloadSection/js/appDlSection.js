a6cfd2c1a43c979923405e43835dc5a33e2308ea
Labana <nlabana@ebay.com>
2016-06-17 13:50:36 -0700

    mock data

--- app/views/components/appDownloadSection/js/appDlSection.js
+++ app/views/components/appDownloadSection/js/appDlSection.js
@@ -1,151 +1,69 @@
 module.exports = {
    
-    'feedTiles': [{
-        'userProfileImageUrl': '/public/images/fake/userProfile.png',
-        'feedText': 'Brian has sold his 1972 Camaro'
-    }, {
-        'userProfileImageUrl': '/public/images/fake/userProfile.png',
-        'feedText': 'Jim has sold his Weber Grill'
-    }, {
-        'userProfileImageUrl': '/public/images/fake/userProfile.png',
-        'feedText': 'Rachel has sold her shoes'
-    }],
+    
     'mostRecentAds': [
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NTQ3WDMwOA==/z/0TkAAOSw41xXO7-v/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Perfecta Encuentro lo que necesito a buen precio.',
+          
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NjAwWDgwMA==/z/P7wAAOSwH71XO7vh/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Es una buena forma de comprar y vende',
+           
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/MTIwMFgxNjAw/z/QQ4AAOSwB9xXO7wa/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Excelente - Me gusto',
+            
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NjAwWDgwMA==/z/-jUAAOSw74FXO8Bv/$_19.jpg',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Excelente App Nunca me he tardado más de 4 días en vender cualquier cosa... Excelente.',
+          
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/MzYzWDMwOA==/z/IsMAAOSwf05XO7~4/$_19.jpg',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Comparando barato Que bueno',
+           
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NjAwWDgwMA==/z/JYgAAOSwf05XO8AO/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Excellent Very good app',
+           
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NDUwWDgwMA==/z/thkAAOSw41xXO76I/$_19.jpg',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Me gusta. Es muy eficaz gracias!!',
+           
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/ODAwWDQ1MA==/z/etMAAOSwf05XO7kY/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Nunca recomiendo nada, esta es genial, el chat ayuda con los interesados',
+           
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NTk4WDgwMA==/z/CioAAOSw41xXO6r6/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Buena aplicacion',
+           
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/Mzc5WDgwMA==/z/5dEAAOSwoudW5uUi/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'El mejor sitio de Internet para hacer negocios, lo recomiendo mucho',
+          
         },
         {
             'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NjAwWDgwMA==/z/3E0AAOSw74FXO78U/$_19.jpg',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'description': 'Esta app es de lo mejor por eso le doy 5 estrellas',
+            
         },
+        
         {
-            'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NDUxWDgwMA==/z/uqYAAOSwfC9XO74I/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
+            'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NjAwWDgwMA==/z/3E0AAOSw74FXO78U/$_19.jpg',
+            'description': 'Esta app es de lo mejor por eso le doy 5 estrellas',
+            
         },
-        {
-            'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NDgwWDgwMA==/z/cm4AAOSwboVXO73h/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
-        },
-        {
-            'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NDA5WDgwMA==/z/W7sAAOSwboVXO7zs/$_19.jpg',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
-        },
-        {
-            'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/ODAwWDQ1MA==/z/6CAAAOSwf05XOoVO/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
-        },
-        {
-            'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NDUwWDgwMA==/z/MpIAAOSwf05XO3sx/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
-        },
-        {
-            'primaryImageUrl': 'http://img.classistatic.com/crop/200x150/i.ebayimg.com/00/s/NDUwWDgwMA==/z/wO0AAOSwfC9XO7Iu/$_19.JPG?set_id=8800005007',
-            'userProfileImageUrl': '/public/images/fake/userProfile.png',
-            'statistics': {
-                'favorCount': Math.floor(Math.random() * 100),
-                'repliesCount': Math.floor(Math.random() * 100)
-            }
-        }
+      
+     
     ]};
 
