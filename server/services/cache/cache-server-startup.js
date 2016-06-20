9d1d91bf28f0e836b04035b869d0683948f98dc3
Mike Soennichsen <msoennichsen@ebay.com>
2016-06-20 10:58:53 -0700

    Removed comments on line about loading file vs bapi config

--- server/services/cache/cache-server-startup.js
+++ server/services/cache/cache-server-startup.js
@@ -90,12 +90,11 @@ function CacheBapiData(siteApp, requestId) {
 	        // Actually return the promise so we know when the app is ready to start.
             return Q(configService.getConfigData(bapiHeaders))
               .then(function (dataReturned) {
-				  //TODO: undo these commented out lines once 1.0 has config update
-                // if (typeof dataReturned.error !== 'undefined' && dataReturned.error !== null) {
+                if (typeof dataReturned.error !== 'undefined' && dataReturned.error !== null) {
                     siteApp.locals.config.bapiConfigData = require(pCwd + '/server/config/bapi/config_' + siteApp.locals.config.locale + '.json');
-                // } else {
-                //     siteApp.locals.config.bapiConfigData = dataReturned;
-                // }
+                } else {
+                    siteApp.locals.config.bapiConfigData = dataReturned;
+                }
 
                 var locationDropdownLevel = siteApp.locals.config.bapiConfigData.header.locationDropdownLevel;
                 var categoryDropdownLevel = siteApp.locals.config.bapiConfigData.header.categoryDropdownLevel;
