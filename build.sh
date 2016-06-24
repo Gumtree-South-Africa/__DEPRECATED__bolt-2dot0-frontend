de2e5276861615feaa3f98be8f82e322538a723c
Meng, Di(dimeng) <dimeng@ebay.com>
2016-06-24 17:48:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,6 +22,8 @@ echo "REGISTRY_HOST=$REGISTRY_HOST"
 sudo docker rmi $REGISTRY_HOST:5000/$IMAGE:$TAG || true
 
 IMAGE_TAG=$TAG-$(date -u +%Y-%m-%d_%H-%M%Z)
-sudo docker build --build-arg GIT_REV_FILE=$GIT_REV_FILE -t "$REGISTRY_HOST:5000/$IMAGE:$IMAGE_TAG" .
+#sudo docker build --build-arg GIT_REV_FILE=$GIT_REV_FILE -t "$REGISTRY_HOST:5000/$IMAGE:$IMAGE_TAG" .
+#--build-arg required higher version docker engine
+sudo docker build -t "$REGISTRY_HOST:5000/$IMAGE:$IMAGE_TAG" .
 sudo docker tag "$REGISTRY_HOST:5000/$IMAGE:$IMAGE_TAG" "$REGISTRY_HOST:5000/$IMAGE:$TAG"
 sudo docker push "$REGISTRY_HOST:5000/$IMAGE:$TAG"
