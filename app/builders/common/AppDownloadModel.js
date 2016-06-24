91b5c7c5969eb93dcba71a72e330fd1eb2a449e8
Labana <nlabana@ebay.com>
2016-06-23 19:01:46 -0700

    adding mock data

---
+++ app/builders/common/AppDownloadModel.js
@@ -0,0 +1,19 @@
+
+'use strict';
+
+class AppDownloadModel {
+    constructor(req, res) {
+        this.req = req;
+        this.res = res;
+    }
+
+    getAppDownload() {
+    	let bapiConfigData = this.res.locals.config.bapiConfigData;
+    	let reviews = bapiConfigData.content.homepageV2.reviews;
+    	console.log(reviews);
+        return reviews;
+    }
+}
+
+module.exports = AppDownloadModel;
+
