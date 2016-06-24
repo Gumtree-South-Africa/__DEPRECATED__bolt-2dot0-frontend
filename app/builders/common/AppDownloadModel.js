037cf7790327dd75c85996a4ced905f66c06cf16
Labana <nlabana@ebay.com>
2016-06-23 20:18:12 -0700

    BOLT-21439

--- app/builders/common/AppDownloadModel.js
+++ app/builders/common/AppDownloadModel.js
@@ -2,16 +2,54 @@
 'use strict';
 
 class AppDownloadModel {
-    constructor(req, res) {
+
+	constructor(req, res) {
         this.req = req;
         this.res = res;
     }
 
     getAppDownload() {
     	let bapiConfigData = this.res.locals.config.bapiConfigData;
-    	let reviews = bapiConfigData.content.homepageV2.reviews;
-    	console.log(reviews);
-        return reviews;
+    	let reviews = bapiConfigData.content.homepageV2.reviews; //array
+    	
+    	let startingIndex = 0, numOfAdsToSend, totalNumOfAds, maxIndexForAds;
+    	let deviceDetection = require(process.cwd() + '/modules/device-detection');
+        
+		if(deviceDetection.isMobile()) {
+			numOfAdsToSend = 1;
+		}
+		else{
+			numOfAdsToSend = 3;
+		}
+	
+		totalNumOfAds = reviews.length;
+		maxIndexForAds = totalNumOfAds -1;
+		
+		if (this.req.cookies['adStartingIndex']) {
+			//read the startingIndex value from cookie and add by numOfAds to display
+			startingIndex = parseInt (this.req.cookies['adStartingIndex']) + numOfAdsToSend;
+		}
+		
+		if (startingIndex > maxIndexForAds)
+		{
+			startingIndex = (startingIndex % maxIndexForAds) - 1; //Index went over, so circling back to beginning.
+		}
+		
+		this.res.cookie('adStartingIndex', startingIndex); // set the new value in the cookie
+		
+		// return three values only
+		if ((startingIndex + numOfAdsToSend) <= totalNumOfAds )
+		{
+			reviews = reviews.slice(startingIndex, startingIndex + numOfAdsToSend);
+		}
+		else
+		{
+			reviews = reviews.slice(startingIndex, totalNumOfAds)
+								.concat(reviews.slice(0, (numOfAdsToSend - (totalNumOfAds - startingIndex))));
+		}
+    	
+    	return reviews;
+    
     }
 }
 
