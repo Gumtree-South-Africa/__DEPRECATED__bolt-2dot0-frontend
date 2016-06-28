3ca1d42ad9d4ea68c4664385c0d7846adda68f06
Gao, George(AWF) <ggao1@ebay.com>
2016-06-27 20:35:19 -0700

    Add profile pics

--- server/services/mockData/recentActivityMock.js
+++ server/services/mockData/recentActivityMock.js
@@ -3,49 +3,49 @@
 module.exports = {
 'recentActivities': [
 {
-'userProfileImageUrl': '/public/images/photo-icon.png',
+'userProfileImageUrl': '/public/images/profile1.png',
 'userFirstName': 'George',
 'categoryId': '68',
 'action': 'listed',
 'prefix1': 'house',
 'prefix2': 'with 3 bedrooms'
 }, {
-'userProfileImageUrl': '/public/images/photo-icon.png',
+'userProfileImageUrl': '/public/images/profile2.png',
 'userFirstName': 'Jim',
 'categoryId': '69',
 'action': 'sold',
 'prefix1': 'honda',
 'prefix2': 'accord'
 }, {
-'userProfileImageUrl': '/public/images/photo-icon.png',
+'userProfileImageUrl': '/public/images/profile6.png',
 'userFirstName': 'Cindy',
 'categoryId': '70',
 'action': 'listed',
 'prefix1': 'macbook pro',
 'prefix2': ''
 }, {
-'userProfileImageUrl': '/public/images/photo-icon.png',
+'userProfileImageUrl': '/public/images/profile3.png',
 'userFirstName': 'Drew',
 'categoryId': '68',
 'action': 'sold',
 'prefix1': 'apartment',
 'prefix2': 'with 2 bedrooms'
 }, {
-'userProfileImageUrl': '/public/images/photo-icon.png',
+'userProfileImageUrl': '/public/images/profile4.png',
 'userFirstName': 'Steve',
 'categoryId': '69',
 'action': 'listed',
 'prefix1': 'BMW',
 'prefix2': 'x3'
 }, {
-'userProfileImageUrl': '/public/images/photo-icon.png',
+'userProfileImageUrl': '/public/images/profile5.png',
 'userFirstName': 'Kevin',
 'categoryId': '70',
 'action': 'sold',
 'prefix1': 'alienware',
 'prefix2': 'X51'
 }, {
-'userProfileImageUrl': '/public/images/photo-icon.png',
+'userProfileImageUrl': '/public/images/profile7.png',
 'userFirstName': 'Susan',
 'categoryId': '68',
 'action': 'listed',
