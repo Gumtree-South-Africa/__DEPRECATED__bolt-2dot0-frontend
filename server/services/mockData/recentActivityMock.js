c0265282cfe779518d2c082b9cda0b410ded3412
Gao, George(AWF) <ggao1@ebay.com>
2016-06-29 10:47:35 -0700

    added i18n

--- server/services/mockData/recentActivityMock.js
+++ server/services/mockData/recentActivityMock.js
@@ -6,6 +6,7 @@ module.exports = {
 'userProfileImageUrl': '/public/images/profile1.png',
 'userFirstName': 'George',
 'categoryId': '68',
+'renderSold': false,
 'action': 'listed',
 'prefix1': 'house',
 'prefix2': 'with 3 bedrooms'
@@ -13,6 +14,7 @@ module.exports = {
 'userProfileImageUrl': '/public/images/profile2.png',
 'userFirstName': 'Jim',
 'categoryId': '69',
+'renderSold': true,
 'action': 'sold',
 'prefix1': 'honda',
 'prefix2': 'accord'
@@ -20,6 +22,7 @@ module.exports = {
 'userProfileImageUrl': '/public/images/profile6.png',
 'userFirstName': 'Cindy',
 'categoryId': '70',
+'renderSold': false,
 'action': 'listed',
 'prefix1': 'macbook pro',
 'prefix2': ''
@@ -27,6 +30,7 @@ module.exports = {
 'userProfileImageUrl': '/public/images/profile3.png',
 'userFirstName': 'Drew',
 'categoryId': '68',
+'renderSold': true,
 'action': 'sold',
 'prefix1': 'apartment',
 'prefix2': 'with 2 bedrooms'
@@ -34,6 +38,7 @@ module.exports = {
 'userProfileImageUrl': '/public/images/profile4.png',
 'userFirstName': 'Steve',
 'categoryId': '69',
+'renderSold': false,
 'action': 'listed',
 'prefix1': 'BMW',
 'prefix2': 'x3'
@@ -41,6 +46,7 @@ module.exports = {
 'userProfileImageUrl': '/public/images/profile5.png',
 'userFirstName': 'Kevin',
 'categoryId': '70',
+'renderSold': true,
 'action': 'sold',
 'prefix1': 'alienware',
 'prefix2': 'X51'
@@ -48,6 +54,7 @@ module.exports = {
 'userProfileImageUrl': '/public/images/profile7.png',
 'userFirstName': 'Susan',
 'categoryId': '68',
+'renderSold': false,
 'action': 'listed',
 'prefix1': 'Steinway',
 'prefix2': 'Baby Grand Piano'
