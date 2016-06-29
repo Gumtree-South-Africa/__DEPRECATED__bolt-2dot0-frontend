5bad86de3a5e985cd053068195ed58f89d55bdd2
Kevin Bek <kbek@ebay.com>
2016-06-29 15:14:15 -0700

    Improving Format of tileGrid.js to better match commonJS pattern

--- app/views/components/tileGrid/js/tileGrid.js
+++ app/views/components/tileGrid/js/tileGrid.js
@@ -5,21 +5,28 @@ let $ = require('jquery');
 let bridget = require('jquery-bridget');
 let Isotope = require('isotope-layout');
 
-// Isotope requres document to be ready activated
-$(document).ready(() => {
 
-	$.bridget('isotope', Isotope);	// after this you can use $().isotope()
+let initialize = () => {
+	// Isotope requres document to be ready activated
+	$(document).ready(() => {
 
-	$('.use-isotope-handler').addClass("using-isotope");	// tag so we get configured sizes
+		$.bridget('isotope', Isotope);	// after this you can use $().isotope()
 
-	$('.use-isotope-handler').isotope({
-		itemSelector: '.tile-item',
-		layoutMode: 'masonry',
-		masonry: {
-			columnWidth: '.column-sizer',
-			gutter: '.gutter-sizer-horizontal',
-			isFitWidth: true
-		}
+		$('.use-isotope-handler').addClass("using-isotope");	// tag so we get configured sizes
+
+		$('.use-isotope-handler').isotope({
+			itemSelector: '.tile-item',
+			layoutMode: 'masonry',
+			masonry: {
+				columnWidth: '.column-sizer',
+				gutter: '.gutter-sizer-horizontal',
+				isFitWidth: true
+			}
+		});
 	});
-});
+};
+
+module.exports = {
+	initialize
+};
 
