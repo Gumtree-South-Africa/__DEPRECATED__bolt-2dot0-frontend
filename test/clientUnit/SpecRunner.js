f44803cf9124771c768902bd9e01362fca0566fe
Kevin Bek <kbek@ebay.com>
2016-06-30 11:09:51 -0700

    Small Comment Update

--- test/clientUnit/SpecRunner.js
+++ test/clientUnit/SpecRunner.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// insert test area into the page
+// insert test area into the page for use by jasmine
 $('body').append('<div id="testArea"></div>');
 
 require('./spec/sampleComponent/sampleCompSpec.js');
