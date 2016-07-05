330df460a7ee7d9431838be30b0073b607931f3c
NACER GARGA <ngarga@ebay.com>
2016-07-05 10:55:02 -0700

    fix implementation of moadal phase 1

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -6,7 +6,7 @@
 module.exports = function watch(gulp, plugins) {
   return function(){
     gulp.task('watch', function() {
-      var stream = gulp.watch('./app/styles/v2/**/**/*.scss', ['sass']);
+      var stream = gulp.watch('./app/styles/v2/**/*.scss', ['sass']);
       return stream;
     })
   }
