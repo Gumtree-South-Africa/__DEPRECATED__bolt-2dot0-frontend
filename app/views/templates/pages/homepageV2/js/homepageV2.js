4db5c33c33d747c981d97dd5536fd8d6fff2e00f
Labana <nlabana@ebay.com>
2016-07-07 10:42:45 -0700

    changes for BOLT-21640

--- app/views/templates/pages/homepageV2/js/homepageV2.js
+++ app/views/templates/pages/homepageV2/js/homepageV2.js
@@ -3,3 +3,4 @@
 // this is where we require what the page needs, so we can bundle per-page
 
 require('app/views/components/tileGrid/js/tileGrid.js').initialize();
+require('app/views/components/buttonmobile/js/buttonmobile.js').initialize();
