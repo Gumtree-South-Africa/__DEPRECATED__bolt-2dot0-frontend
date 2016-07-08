a1ae25c76aae3b46ee22f3994db57cce805ff016
Dennis ONeill <denoneill@ebay.com>
2016-07-07 17:22:49 -0700

    added styling for header and chevron icon on "browse"

---
+++ scripts/rebuildMinSvg.sh
@@ -0,0 +1,6 @@
+rm ../public/css/es_MX/svg/sprite.css-1694009d.svg
+rm ../public/css/es_MX/es_MX.css
+rm ../public/css/es_MX/es_MX.png
+rm ../public/css/es_MX/sprite.css
+rm ../public/css/es_MX/sprite.es_MX.html
+gulp svgSprite
