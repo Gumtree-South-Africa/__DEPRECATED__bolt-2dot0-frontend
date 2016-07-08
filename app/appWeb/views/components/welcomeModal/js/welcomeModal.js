2a98540a4c7180125e7c3c5b18fb15e7e32b86c0
Labana <nlabana@ebay.com>
2016-07-07 22:55:59 -0700

    BOLT-21640

--- app/appWeb/views/components/welcomeModal/js/welcomeModal.js
+++ app/appWeb/views/components/welcomeModal/js/welcomeModal.js
@@ -6,27 +6,27 @@ let initialize = () => {
 $(document).ready(() => {
 	
 	function getCookie(cname) {
-	    var name = cname + "=";
-	    var ca = document.cookie.split(';'); //cookie array
-	    for(var i = 0; i <ca.length; i++) {
-	        var c = ca[i];
-	        while (c.charAt(0)==' ') {
+	    let name = cname + "=";
+	    let ca = document.cookie.split(';'); //cookie array
+	    for(let i = 0; i <ca.length; i++) {
+	        let c = ca[i];
+	        while (c.charAt(0) ===' ') {
 	            c = c.substring(1);
 	        }
-	        if (c.indexOf(name) == 0) {
+	        if (c.indexOf(name) === 0) {
 	            return c.substring(name.length,c.length);
 	        }
 	    }
 	    return "";
 	   }
 	  
-	   if (getCookie('alreadyVisited') == "") {
+	   if (getCookie('alreadyVisited') === "") {
 		    document.cookie = 'alreadyVisited=true';
 		    $('.modal-wrapper .modal').css('display', 'block');
         }	
 	
 	
-		$('.modal-wrapper .modal-close-section').on('click', function(){
+		$('.modal-wrapper .modal-close-section').on('click', function() {
              $('.modal-wrapper .modal').fadeOut('slow', function() {
 		         $(this).removeClass('modal');
 		         $('.modal-footer').css('display', 'block');
--- app/appWeb/views/templates/pages/homepageV2/js/homepageV2.js
+++ app/appWeb/views/templates/pages/homepageV2/js/homepageV2.js
@@ -2,5 +2,5 @@
 
 // this is where we require what the page needs, so we can bundle per-page
 
-require('app/views/components/tileGrid/js/tileGrid.js').initialize();
-require('app/views/components/welcomeModal/js/welcomeModal.js').initialize();
+require('app/appWeb/views/components/tileGrid/js/tileGrid.js').initialize();
+require('app/appWeb/views/components/welcomeModal/js/welcomeModal.js').initialize();
