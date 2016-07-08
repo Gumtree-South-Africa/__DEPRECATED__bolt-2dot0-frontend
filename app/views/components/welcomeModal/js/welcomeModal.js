3f9dfa70617432e324d1f7b4bb169e2d47815409
Labana <nlabana@ebay.com>
2016-07-07 21:53:24 -0700

    implementation of welcome screen

---
+++ app/views/components/welcomeModal/js/welcomeModal.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+let initialize = () => {
+
+$(document).ready(() => {
+	
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';'); //cookie array
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	            return c.substring(name.length,c.length);
+	        }
+	    }
+	    return "";
+	   }
+	  
+	   if (getCookie('alreadyVisited') == "") {
+		    document.cookie = 'alreadyVisited=true';
+		    $('.modal-wrapper, .modal').css('display', 'block');
+        }	
+	
+	
+		$('.modal-wrapper .modal-close-section').on('click', function(){
+            
+	         $('.modal-wrapper, .modal').fadeOut('slow', function() {
+	         $(this).removeClass('modal');
+	         $('.modal-footer').css('display', 'block');
+	       });
+		 });
+
+
+
+     });
+ };
+
+module.exports = {
+initialize
+};
--- app/views/templates/pages/homepageV2/js/homepageV2.js
+++ app/views/templates/pages/homepageV2/js/homepageV2.js
@@ -3,4 +3,4 @@
 // this is where we require what the page needs, so we can bundle per-page
 
 require('app/views/components/tileGrid/js/tileGrid.js').initialize();
-require('app/views/components/buttonmobile/js/buttonmobile.js').initialize();
+require('app/views/components/welcomeModal/js/welcomeModal.js').initialize();
