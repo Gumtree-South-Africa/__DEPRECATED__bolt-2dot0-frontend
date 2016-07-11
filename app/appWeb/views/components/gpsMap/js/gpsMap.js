ef77f66727e55be1eba3882b3a265017666eef73
Videep Raj <vrajendiran@ebay.com>
2016-07-11 01:01:58 -0700

    add clustering algorithm, remove default controls on maps

--- app/appWeb/views/components/gpsMap/js/gpsMap.js
+++ app/appWeb/views/components/gpsMap/js/gpsMap.js
@@ -4,18 +4,19 @@ let initialize = () => {
 
 		$(document).ready(() => {
 			function initMap(data) {
-
 				let mapDiv = document.getElementById('map');
 				let map = new google.maps.Map(mapDiv, {
 					center: {
 						lat: 19.414980,
 						lng: -99.177446
 					},
-					zoom: 12
+					zoom: 14,
+					disableDefaultUI: true
 				});
 
 				let markers = [];
-				for (let i = 0; i < 100; i++) {
+				/*
+				for (let i = 0; i < data.length; i++) {
 					let latLng = new google.maps.LatLng(data.photos[i].latitude, data.photos[i].longitude);
 					let markerImage = new google.maps.MarkerImage(data.photos[i].imageUrl, new google.maps.Size(24, 32));
 					let marker = new google.maps.Marker({
@@ -25,6 +26,7 @@ let initialize = () => {
 					});
 					markers.push(marker);
 				}
+				*/
 
 				let mcOptions = {
 					gridSize: 50,
--- app/appWeb/views/templates/pages/homepageV2/js/homepageV2.js
+++ app/appWeb/views/templates/pages/homepageV2/js/homepageV2.js
@@ -3,4 +3,5 @@
 // this is where we require what the page needs, so we can bundle per-page
 
 require('app/appWeb/views/components/tileGrid/js/tileGrid.js').initialize();
+require('app/appWeb/views/components/gpsMap/js/markerClusterer.js');
 require('app/appWeb/views/components/gpsMap/js/gpsMap.js').initialize();
