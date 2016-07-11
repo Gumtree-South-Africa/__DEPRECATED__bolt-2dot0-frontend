ee7db46542e0fdc75a324477ba401ad3709b74c8
Mike Soennichsen <msoennichsen@ebay.com>
2016-07-11 15:30:54 -0700

    Fixed the click and hover events on the dropdown menus

--- app/appWeb/views/components/headerV2/js/header.js
+++ app/appWeb/views/components/headerV2/js/header.js
@@ -2,27 +2,43 @@
 
 let $ = require('jquery');
 
-let _toggleBrowseMenu = () => {
-    $('#js-cat-dropdown').toggleClass('hidden');
-    $('#js-browse-dropdown #js-browse-item-text').toggleClass('menu-open');
-    $('#js-browse-header-item-icon').toggleClass('icon-down');
-    $('#js-browse-header-item-icon').toggleClass('icon-up');
+let _toggleBrowseMenu = (shouldClose) => {
+	if (shouldClose === undefined) {
+		shouldClose = !$('#js-cat-dropdown').hasClass('hidden');
+	}
+    $('#js-cat-dropdown').toggleClass('hidden', shouldClose);
+	$('#js-browse-dropdown #js-browse-item-text').toggleClass('menu-open', !shouldClose);
+	$('#js-browse-header-item-icon').toggleClass('icon-down', shouldClose);
+	$('#js-browse-header-item-icon').toggleClass('icon-up', !shouldClose);
 };
 
-let _toggleProfileMenu = () => {
-    $('#js-profile-dropdown').toggleClass('hidden');
-    $('#js-profile-item-text').toggleClass('menu-open');
-    $('#js-profile-header-item-icon').toggleClass('icon-down');
-    $('#js-profile-header-item-icon').toggleClass('icon-up');
+let _toggleProfileMenu = (shouldClose) => {
+	if (shouldClose === undefined) {
+		shouldClose = !$('#js-profile-dropdown').hasClass('hidden');
+	}
+	$('#js-profile-dropdown').toggleClass('hidden', shouldClose);
+	$('#js-profile-item-text').toggleClass('menu-open', !shouldClose);
+	$('#js-profile-header-item-icon').toggleClass('icon-down', shouldClose);
+	$('#js-profile-header-item-icon').toggleClass('icon-up', !shouldClose);
 };
 
 let initialize = () => {
 	$(document).ready(() => {
-		$('.browse').on('tap', _toggleBrowseMenu)
-			.hover(_toggleBrowseMenu, _toggleBrowseMenu);
+		$('.browse').on('click', () => {
+			_toggleBrowseMenu();
+		}).mouseenter(() => {
+			_toggleBrowseMenu(false);
+		}).mouseleave(() => {
+			_toggleBrowseMenu(true);
+		});
 
-		$('.profile').on('tap', _toggleProfileMenu)
-			.hover(_toggleProfileMenu, _toggleProfileMenu);
+		$('.profile').on('click', () => {
+			_toggleProfileMenu();
+		}).mouseenter(() => {
+			_toggleProfileMenu(false);
+		}).mouseleave(() => {
+			_toggleProfileMenu(true);
+		});
 	});
 };
 
