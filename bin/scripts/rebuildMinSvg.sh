c8ff7637ae01dbb8afb1dbe533ca83614f7fa964
Mike Soennichsen <msoennichsen@ebay.com>
2016-07-11 17:52:56 -0700

    Moved rebuild script to bin/scripts

---
+++ bin/scripts/rebuildMinSvg.sh
@@ -0,0 +1,6 @@
+rm ../../public/css/es_MX/svg/sprite.css-*.svg
+rm ../../public/css/es_MX/es_MX.css
+rm ../../public/css/es_MX/es_MX.png
+rm ../../public/css/es_MX/sprite.css
+rm ../../public/css/es_MX/sprite.es_MX.html
+gulp icons2
