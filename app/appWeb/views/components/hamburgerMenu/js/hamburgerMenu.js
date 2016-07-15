b5642c85fc1c9fd7592d56f7700ccddb22b787b8
Mike Soennichsen <msoennichsen@ebay.com>
2016-07-15 14:33:59 -0700

    Fixed resize issue with open menu, close autocomplete on menu open, fixed autocomplete position issue with position:relative

--- app/appWeb/views/components/hamburgerMenu/js/hamburgerMenu.js
+++ app/appWeb/views/components/hamburgerMenu/js/hamburgerMenu.js
@@ -1,6 +1,10 @@
 'use strict';
+let searchBarV2 = require('app/appWeb/views/components/searchbarV2/js/searchbarV2.js');
 
 let _toggleMenu = () => {
+	if (!this.open) {
+		searchBarV2.closeAutoComplete();
+	}
 	this.$hamburgerPopout[0].style.right = (this.open) ? '100%' : 0;
 	this.$hamburgerContents.animate({
 		left: (this.open) ? '-70%' : '0',
@@ -9,6 +13,9 @@ let _toggleMenu = () => {
 	this.$body.toggleClass('overflow-hidden');
 	this.$modalFooter.toggleClass('hidden');
 	this.$pageContent.animate({
+		marginLeft: (this.open) ? 0 : '70%'
+	});
+	this.$header.animate({
 		left: (this.open) ? 0 : '70%'
 	});
 	this.$pageContent.toggleClass('menu-closed');
@@ -22,6 +29,18 @@ let _toggleBrowseCategory = () => {
 	$('li', this.$browse).toggleClass('hidden');
 };
 
+let _isBreakpotint = (alias) => {
+	return $(`#js-is-${alias}`, this.$hamburgerContents).is(":visible");
+};
+
+let _currentBreakpoint = () => {
+	if (_isBreakpotint('mobile')) {
+		return 'mobile';
+	} else if (_isBreakpotint('desktop')) {
+		return 'desktop';
+	}
+};
+
 let initialize = () => {
 	this.$body = $('body');
 	this.open = false;
@@ -29,10 +48,27 @@ let initialize = () => {
 	this.$browseArrow = $('#js-browse-arrow', this.$browse);
 	this.$browseCategories = $('#js-browse-categories', this.$browse);
 	this.$modalFooter = $('.modal-footer');
-	this.$pageContent = $('.headerV2, .containment');
+	this.$header = $('.headerV2');
+	this.$pageContent = $('.containment');
 	this.$hamburgerPopout = $('#js-body-overlay');
 	this.$hamburgerIcon = $('#js-hamburger-icon');
 	this.$hamburgerContents = $('.hamburger-contents');
+	this.currentWindowSize = _currentBreakpoint();
+	
+	this.$body.trigger('viewportChanged', this.currentWindowSize);
+	$(window).bind('resize', () => {
+		let newWindowSize = _currentBreakpoint();
+		if (newWindowSize !== this.currentWindowSize) {
+			this.$body.trigger('viewportChanged', [newWindowSize, this.currentWindowSize]);
+			this.currentWindowSize = newWindowSize;
+		}
+	});
+	
+	this.$body.on('viewportChanged', (event, newSize, oldSize) => {
+		if (newSize === 'desktop' && oldSize === 'mobile' && this.open) {
+			_toggleMenu();
+		}
+	});
 
 	this.$pageContent.addClass('open-menu');
 	this.$browseCategories.on('click', _toggleBrowseCategory);
