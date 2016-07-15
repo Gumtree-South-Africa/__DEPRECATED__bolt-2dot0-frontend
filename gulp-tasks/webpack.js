85369691f0b68d39b9161154791bcba3401ca8df
Kevin Bek <kbek@ebay.com>
2016-07-15 10:02:10 -0700

    BOLT-22189 Infrastructure to support client side testing, specifically precompiling templates

--- gulp-tasks/webpack.js
+++ gulp-tasks/webpack.js
@@ -5,11 +5,18 @@ let	shell = require('gulp-shell');
 // ////////////////////////////////////////////////
 // webpack Task
 // ///////////////////////////////////////////////
-module.exports = function webpack(gulp, plugins) {
+module.exports = function webpack(gulp) {
 	return function(){
+		let webpack = require("webpack");
 		// CLIENT UNIT TEST TASKS
 		gulp.task('webpack', (done) => {
-			return shell.task(["node node_modules/webpack/bin/webpack.js"])(done);
+			// run webpack
+			webpack(require(process.cwd() + "/webpack.config.js"), function(err, stats) {
+				if(err) throw new gutil.PluginError("webpack", err);
+				console.log("[webpack]", stats.toString());
+
+				done();
+			});
 		});
 	}
 };
--- test/clientUnit/helpers/commonSpecHelper.js
+++ test/clientUnit/helpers/commonSpecHelper.js
@@ -1,46 +1,13 @@
 'use strict';
 
+
+let $ = require("jquery");
+let clientHbsHelpers = require("./clientHbsHelpers.js");
+
 let mockAjaxMapQueue = {};
 
-/**
- * enqueue to mockAjaxMapQueue by url
- * @param {string} url - url endpoint that will be queued
- * @param {object} returnData - returnData to return on dequeue
- */
-let enqueue = (url, returnData) => {
-	if (mockAjaxMapQueue.hasOwnProperty(url) && Array.isArray(mockAjaxMapQueue[url])) {
-		mockAjaxMapQueue[url].push(returnData);
-	} else if (!mockAjaxMapQueue.hasOwnProperty(url)) {
-		mockAjaxMapQueue[url] = [returnData];
-	}
-};
-
-/**
- * dequeue item from mockAjaxMapQueue by url
- * @param {string} url - url endpoint to get data from
- * @returns {object} - mocked data
- */
-let dequeue = (url) => {
-	if (!mockAjaxMapQueue.hasOwnProperty(url) || !Array.isArray(mockAjaxMapQueue[url])) {
-		throw Error('No Registered Mock Ajax for url -> ' + url);
-	}
-
-	let returnData = mockAjaxMapQueue[url].shift();
-	if (mockAjaxMapQueue[url].length <= 0) {
-		delete mockAjaxMapQueue[url];
-	}
-
-	return returnData;
-};
-
-/**
- * Given url and return data, store the mock for use later
- * @param {string} url - url to mock
- * @param {object} returnData - data to be returned from mocked ajax request
- */
-module.exports.registerMockAjax = (url, returnData) => {
-	enqueue(url, returnData);
-};
+// fix HBS Template Helpers for the Client
+clientHbsHelpers.initialize();
 
 /**
  * Prepare client template and return that DOM after appending to the screen
@@ -48,8 +15,8 @@ module.exports.registerMockAjax = (url, returnData) => {
  * @param {object} templateModel - model to use in template render
  * @returns {jQuery} return testArea
  */
-module.exports.prepareTemplate = (templateName, templateModel) => {
-	let template = Handlebars.templates[templateName];
+let _prepareTemplate = (templateName, templateModel) => {
+	let template = Handlebars.partials[templateName];
 
 	if (!template) {
 		throw Error(`No precompiled template with the name -> ${templateName}`);
@@ -60,10 +27,82 @@ module.exports.prepareTemplate = (templateName, templateModel) => {
 	return $('#testArea').append(dom);
 };
 
+/**
+ * enqueue to mockAjaxMapQueue by url
+ * @param {string} url - url endpoint that will be queued
+ * @param {object} returnData - returnData to return on dequeue
+ */
+let _enqueue = (url, returnData, options) => {
+	if (mockAjaxMapQueue.hasOwnProperty(url) && Array.isArray(mockAjaxMapQueue[url])) {
+		mockAjaxMapQueue[url].push({
+			returnData,
+			options
+		});
+	} else if (!mockAjaxMapQueue.hasOwnProperty(url)) {
+		mockAjaxMapQueue[url] = [{
+			returnData,
+			options
+		}];
+	}
+};
+
+/**
+ * dequeue item from mockAjaxMapQueue by url
+ * @param {string} url - url endpoint to get data from
+ * @returns {object} - mocked data
+ */
+let _dequeue = (url) => {
+	if (!mockAjaxMapQueue.hasOwnProperty(url) || !Array.isArray(mockAjaxMapQueue[url])) {
+		throw Error('No Registered Mock Ajax for url -> ' + url);
+	}
+
+	let ajaxInfo = mockAjaxMapQueue[url].shift();
+	if (mockAjaxMapQueue[url].length <= 0) {
+		delete mockAjaxMapQueue[url];
+	}
+
+	return ajaxInfo;
+};
+
+let simulateTextInput = ($input, text) => {
+	$input.val(text);
+	$input.trigger("input").trigger("keyup").focus();
+};
+
+/**
+ * Given url and return data, store the mock for use later
+ * @param {string} url - url to mock
+ * @param {object} returnData - data to be returned from mocked ajax request
+ */
+let registerMockAjax = (url, returnData, options) => {
+	_enqueue(url, returnData, options);
+};
+
+let setupTest = (templateName, templateModel, locale) => {
+	clientHbsHelpers.setLocale(locale);
+	return _prepareTemplate(templateName, templateModel);
+};
+
+module.exports = {
+	simulateTextInput,
+	setupTest,
+	registerMockAjax
+};
+
 // spying on ajax and replacing with fake, mock function
 beforeEach(() => {
 	spyOn($, 'ajax').and.callFake((options) => {
-		let data = dequeue(options.url);
-		options.success(data);
+		let ajaxInfo = _dequeue(options.url);
+
+		if (ajaxInfo.options) {
+			if (ajaxInfo.delay && Number.isInteger(ajaxInfo.delay)) {
+				setTimeout(() => {
+					options.success(ajaxInfo.returnData);
+				}, ajaxInfo.delay);
+			}
+		} else {
+			options.success(ajaxInfo.returnData);
+		}
+
 	});
 });
