b270503502f829103ea62315a031d2243b75b782
Gao, George(AWF) <ggao1@ebay.com>
2016-07-22 14:31:25 -0700

    remove specs

--- test/serverUnit/spec/topLocationsSpec.js
+++ test/serverUnit/spec/topLocationsSpec.js
@@ -2,37 +2,8 @@
 let specHelper = require('../helpers/specHelper');
 let boltSupertest = specHelper.boltSupertest;
 let cheerio = require('cheerio');
-let endpoints = require(`${process.cwd()}/server/config/mock.json`).BAPI.endpoints;
 
 describe('Top Locations', () => {
-	let i18n;
-
-	beforeAll(() => {
-		i18n = specHelper.getMockDataByLocale("/app/locales/bolt-translation", "", "es_MX");
-	});
-
-	beforeEach(() => {
-		specHelper.registerMockEndpoint(
-			`${endpoints.topLocationsL2}?_forceExample=true&_statusCode=200`,
-			'test/serverUnit/mockData/api/v1/LocationList.json');
-		specHelper.registerMockEndpoint(
-			`${endpoints.topKeywords}?limit=15&_forceExample=true&_statusCode=200`,
-			'test/serverUnit/mockData/api/v1/keywords.json');
-		specHelper.registerMockEndpoint(
-			`${endpoints.trendingKeywords}?limit=15&_forceExample=true&_statusCode=200`,
-			'test/serverUnit/mockData/api/v1/keywords.json');
-		specHelper.registerMockEndpoint(
-			`${endpoints.homepageGallery}?_forceExample=true&_statusCode=200`,
-			'test/serverUnit/mockData/api/v1/GallerySlice.json');
-		specHelper.registerMockEndpoint(
-			`${endpoints.adStatistics}?_forceExample=true&_statusCode=200`,
-			'test/serverUnit/mockData/api/v1/GallerySlice.json');
-		specHelper.registerMockEndpoint(
-			`/users/logged-in-user-info?_forceExample=true&_statusCode=200`,
-			'test/serverUnit/mockData/api/v1/UserHeaderInfo.json');
-	});
-
-
 	it('should show locations', (done) => {
 		boltSupertest('/', 'vivanuncios.com.mx').then((supertest) => {
 			supertest
