974e8cfc9be02e42ba46d8150cdf114f41fbbe58
NACER GARGA <ngarga@ebay.com>
2016-07-22 14:52:19 -0700

    auto intend

--- app/appWeb/views/components/modal/js/locationPrompt.js
+++ app/appWeb/views/components/modal/js/locationPrompt.js
@@ -4,66 +4,64 @@ let initialize = () => {
 
 	$(document).ready(() => {
 
-
 		function geoShowMyLocation(geoCookieValue) {
 			geoCookieValue = geoCookieValue.replace('ng',',');
 
 			$.ajax({
-					url: 'https://maps.google.com/maps/api/geocode/json?latlng=' + geoCookieValue + '&sensor=false',
-					dataType: 'JSON',
-					type: 'GET',
-					success: function(resp) {
-						//console.log(resp);
-							$('#modal-location').removeClass('spinner').attr('disabled', false);
-							if (resp.results instanceof Array) {
-								if(resp.results.length > 1) {
-									$('.search-textbox-container .location-text').html(resp.results[resp.results.length-2].address_components[0].long_name);
-									$('#modal-location').val(resp.results[resp.results.length-2].address_components[0].long_name);
-								} else {
-									$('.search-textbox-container .location-text').html(resp.results[resp.results.length-1].address_components[0].long_name);
-									$('#modal-location').val(resp.results[resp.results.length-1].address_components[0].long_name);
-								}
-							}
+				url: 'https://maps.google.com/maps/api/geocode/json?latlng=' + geoCookieValue + '&sensor=false',
+				dataType: 'JSON',
+				type: 'GET',
+				success: function(resp) {
+					$('#modal-location').removeClass('spinner').attr('disabled', false);
+					if (resp.results instanceof Array) {
+						if(resp.results.length > 1) {
+							$('.search-textbox-container .location-text').html(resp.results[resp.results.length-2].address_components[0].long_name);
+							$('#modal-location').val(resp.results[resp.results.length-2].address_components[0].long_name);
+						} else {
+							$('.search-textbox-container .location-text').html(resp.results[resp.results.length-1].address_components[0].long_name);
+							$('#modal-location').val(resp.results[resp.results.length-1].address_components[0].long_name);
+						}
 					}
+				}
 			});
 		}
 
-    function geoFindMe() {
+		function geoFindMe() {
 
-      if (!navigator.geolocation) {
+			if (!navigator.geolocation) {
 				console.error('Geolocation is not supported by your browser');
-        return;
-      }
+				return;
+			}
 
-      function success(position) {
-        let latitude  = position.coords.latitude;
-        let longitude = position.coords.longitude;
+			function success(position) {
+				let latitude  = position.coords.latitude;
+				let longitude = position.coords.longitude;
 
 				document.cookie = 'geoId' + "=" + escape(latitude + 'ng' + longitude) + ";path=/";
 				geoShowMyLocation(escape(latitude + 'ng' + longitude));
 
-      }
+			}
 
-      function error() {
+			function error() {
 				console.error('Unable to retrieve your location');
-      }
+			}
 
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
+			navigator.geolocation.getCurrentPosition(success, error);
+		}
 
 		function getCookie(cname) {
-		    let name = cname + "=";
-		    let ca = document.cookie.split(';');
-		    for(let i = 0; i <ca.length; i++) {
-		        let c = ca[i];
-		        while (c.charAt(0)===' ') {
-		            c = c.substring(1);
-		        }
-		        if (c.indexOf(name) === 0) {
-		            return c.substring(name.length,c.length);
-		        }
-		    }
-		    return "";
+			let name = cname + "=";
+			let ca = document.cookie.split(';');
+			for(let i = 0; i <ca.length; i++) {
+				let c = ca[i];
+				while (c.charAt(0)===' ') {
+					c = c.substring(1);
+				}
+				if (c.indexOf(name) === 0) {
+					return c.substring(name.length,c.length);
+				}
+			}
+			return "";
 		}
 
 
@@ -76,13 +74,13 @@ let initialize = () => {
 
 		//click on Continue button
 		$('.modal-cp .btn').on('click', function() {
-				geoShowMyLocation(getCookie('geoId'));
+			geoShowMyLocation(getCookie('geoId'));
 		});
 
 		//on Initialize
 		geoShowMyLocation(getCookie('geoId'));
 
-  });
+	});
 };
 
 
