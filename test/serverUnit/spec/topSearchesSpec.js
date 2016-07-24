fad0e61b259b746fc4b91e783a99e77a29bd281d
Gao, George(AWF) <ggao1@ebay.com>
2016-07-23 22:39:28 -0700

    add i18n to serverSpecs

--- test/serverUnit/spec/topSearchesSpec.js
+++ test/serverUnit/spec/topSearchesSpec.js
@@ -2,8 +2,55 @@
 let specHelper = require('../helpers/specHelper');
 let boltSupertest = specHelper.boltSupertest;
 let cheerio = require('cheerio');
+let endpoints = require(`${process.cwd()}/server/config/mock.json`).BAPI.endpoints;
+
 
 describe('Top Searches', () => {
+	let i18n;
+
+	beforeAll(() => {
+		i18n = specHelper.getMockDataByLocale("/app/locales/bolt-translation", "", "es_MX");
+	});
+
+	beforeEach(() => {
+		specHelper.registerMockEndpoint(
+			`${endpoints.topLocationsL2}?_forceExample=true&_statusCode=200`,
+			'test/serverUnit/mockData/api/v1/LocationList.json');
+		specHelper.registerMockEndpoint(
+			`${endpoints.topKeywords}?limit=15&_forceExample=true&_statusCode=200`,
+			'test/serverUnit/mockData/api/v1/keywords.json');
+		specHelper.registerMockEndpoint(
+			`${endpoints.trendingKeywords}?limit=15&_forceExample=true&_statusCode=200`,
+			'test/serverUnit/mockData/api/v1/keywords.json');
+		specHelper.registerMockEndpoint(
+			`${endpoints.homepageGallery}?_forceExample=true&_statusCode=200`,
+			'test/serverUnit/mockData/api/v1/GallerySlice.json');
+		specHelper.registerMockEndpoint(
+			`${endpoints.adStatistics}?_forceExample=true&_statusCode=200`,
+			'test/serverUnit/mockData/api/v1/GallerySlice.json');
+		specHelper.registerMockEndpoint(
+			`/users/logged-in-user-info?_forceExample=true&_statusCode=200`,
+			'test/serverUnit/mockData/api/v1/UserHeaderInfo.json');
+	});
+
+	it('top searches should have correct i18n content', (done) => {
+		boltSupertest('/', 'vivanuncios.com.mx').then((supertest) => {
+			supertest
+				.set('Cookie', 'b2dot0Version=2.0')
+				.expect((res) => {
+					expect(res.status).toBe(200);
+
+					let c$ = cheerio.load(res.text);
+					let searchHeader = c$('.search-header-text');
+					expect(searchHeader.length).toBe(1, 'selector should produce 1 element for search header');
+
+					let searchHeaderText = c$('.search-header-text').text().trim();
+					expect(searchHeaderText).toBe(i18n.home.popular.searches, 'i18n string for search header item should match');
+				})
+				.end(specHelper.finish(done));
+		});
+	});
+
 	it('should show top searches', (done) => {
 		boltSupertest('/', 'vivanuncios.com.mx').then((supertest) => {
 			supertest
