2ca9feff2030642fa542df2370194f728d47c363
Videep Raj <vrajendiran@ebay.com>
2016-07-25 18:09:08 -0700

    add prefix framework for feed

--- app/builders/common/RecentActivityModel.js
+++ app/builders/common/RecentActivityModel.js
@@ -7,14 +7,33 @@ class RecentActivityModel {
 		this.bapiHeaderValues = bapiHeaderValues;
 	}
 
+	getPrefix1(feed) {
+		return feed.action;
+	}
+
+	getPrefix2(feed) {
+		return feed.action;
+	}
+
 	getRecentActivities(geoLatLng) {
 		return recentActivityService.getRecentActivities(this.bapiHeaderValues, geoLatLng).then((data) => {
 			data.recent = [];
 
 			if (data.ads instanceof Array && data.ads.length>0) {
-				data.recent.push(data.ads[Math.floor(Math.random() * data.ads.length)]);
-				data.recent.push(data.ads[Math.floor(Math.random() * data.ads.length)]);
-				data.recent.push(data.ads[Math.floor(Math.random() * data.ads.length)]);
+				let feed1 = data.ads[Math.floor(Math.random() * data.ads.length)];
+				feed1.prefix1 = '11';
+				feed1.prefix2 = '12';
+				data.recent.push(feed1);
+
+				let feed2 = data.ads[Math.floor(Math.random() * data.ads.length)];
+				feed2.prefix1 = '21';
+				feed2.prefix2 = '22';
+				data.recent.push(feed2);
+
+				let feed3 = data.ads[Math.floor(Math.random() * data.ads.length)];
+				feed3.prefix1 = '31';
+				feed3.prefix2 = '32';
+				data.recent.push(feed3);
 			}
 
 			return data;
