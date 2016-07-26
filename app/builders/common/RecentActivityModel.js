283db37f2dc259f51ae3ca736f6472ef4de97eba
Videep Raj <vrajendiran@ebay.com>
2016-07-25 23:28:35 -0700

    set renderSold

--- app/builders/common/RecentActivityModel.js
+++ app/builders/common/RecentActivityModel.js
@@ -15,22 +15,29 @@ class RecentActivityModel {
 		return feed.action;
 	}
 
+	isSold(feed) {
+		return !(feed.action === 'LISTED');
+	}
+
 	getRecentActivities(geoLatLng) {
 		return recentActivityService.getRecentActivities(this.bapiHeaderValues, geoLatLng).then((data) => {
 			data.recent = [];
 
 			if (data.ads instanceof Array && data.ads.length>0) {
 				let feed1 = data.ads[Math.floor(Math.random() * data.ads.length)];
+				feed1.renderSold = this.isSold(feed1);
 				feed1.prefix1 = '11';
 				feed1.prefix2 = '12';
 				data.recent.push(feed1);
 
 				let feed2 = data.ads[Math.floor(Math.random() * data.ads.length)];
+				feed2.renderSold = this.isSold(feed2);
 				feed2.prefix1 = '21';
 				feed2.prefix2 = '22';
 				data.recent.push(feed2);
 
 				let feed3 = data.ads[Math.floor(Math.random() * data.ads.length)];
+				feed3.renderSold = this.isSold(feed3);
 				feed3.prefix1 = '31';
 				feed3.prefix2 = '32';
 				data.recent.push(feed3);
