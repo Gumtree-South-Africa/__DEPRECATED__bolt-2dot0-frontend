9b00b0398ea1fd2cce8829abc40d2b8aea353e69
Videep Raj <vrajendiran@ebay.com>
2016-07-26 18:23:21 -0700

    integrate trending card BAPI

--- server/services/cardService.js
+++ server/services/cardService.js
@@ -1,25 +1,20 @@
 'use strict';
-// let config = require('config');
-// let bapiOptionsModel = require("./bapi/bapiOptionsModel");
-// let bapiService      = require("./bapi/bapiService");
+
+let config = require('config');
+let bapiOptionsModel = require("./bapi/bapiOptionsModel");
+let bapiService      = require("./bapi/bapiService");
 
 /**
  * Gets data based on the endpoint and parameters passed
  */
 class CardService {
 
-	// this is a temporary hack because mock services are not available in other environments
-	getCardItemsData() {
-		return require('q')(require(process.cwd() + '/server/services/mockData/Ads.json'));
+	getCardItemsData(bapiHeaderValues, queryEndpoint, parameters) {
+	 	return bapiService.bapiPromiseGet(bapiOptionsModel.initFromConfig(config, {
+	 		method: 'GET',
+	 		path: config.get(queryEndpoint),
+	 		extraParameters: parameters,    // bapiOptionsModel may bring 'parameters' in from config, so we use extraParameters
+	 	}), bapiHeaderValues, 'card');
 	}
-
-	// getCardItemsData(bapiHeaderValues, queryEndpoint, parameters) {
-    //
-	// 	return bapiService.bapiPromiseGet(bapiOptionsModel.initFromConfig(config, {
-	// 		method: 'GET',
-	// 		path: config.get(queryEndpoint),
-	// 		extraParameters: parameters,    // bapiOptionsModel may bring 'parameters' in from config, so we use extraParameters
-	// 	}), bapiHeaderValues, 'card');
-	// }
 }
 module.exports = new CardService();
