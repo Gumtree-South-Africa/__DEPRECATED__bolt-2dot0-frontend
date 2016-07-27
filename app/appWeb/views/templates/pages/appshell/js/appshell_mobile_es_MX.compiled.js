1616d5d7047bce67a19d246dd8084c02cf4a7e83
Videep Raj <vrajendiran@ebay.com>
2016-07-27 12:17:04 -0700

    Revert "[Prepare Release] Bumped version number --> app: 2.0.114, static: 2.0.114"
    This reverts commit 995b316f0d6b135e107f0ba9e1c3b18d46abc611.

--- app/appWeb/views/templates/pages/appshell/js/appshell_mobile_es_MX.compiled.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-require("app/appWeb/views/components/headerV2/js/header.js").initialize();
-require("app/appWeb/views/components/footerV2/js/footer.js").initialize();
-require("app/appWeb/views/components/sectionMenu/js/sectionMenu.js").initialize();
-
