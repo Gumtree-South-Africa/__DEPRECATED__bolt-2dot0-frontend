995b316f0d6b135e107f0ba9e1c3b18d46abc611
Node-Jenkins-VM <vrajendiran@ebay.com>
2016-07-27 10:10:24 -0700

    [Prepare Release] Bumped version number --> app: 2.0.114, static: 2.0.114

---
+++ app/appWeb/views/templates/pages/appshell/js/appshell_desktop_es_MX.compiled.js
@@ -0,0 +1,6 @@
+"use strict";
+
+require("app/appWeb/views/components/headerV2/js/header.js").initialize();
+require("app/appWeb/views/components/footerV2/js/footer.js").initialize();
+require("app/appWeb/views/components/sectionMenu/js/sectionMenu.js").initialize();
+
