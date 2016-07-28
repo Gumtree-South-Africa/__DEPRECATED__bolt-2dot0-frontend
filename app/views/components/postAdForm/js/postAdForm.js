fa007cfd80903ff0957d86288e8c7ff3fbec3a5d
Ching, Raymond <raching@ebay.com>
2016-07-28 11:33:40 -0700

    Post Ad - Wired up character count for title input

--- app/views/components/postAdForm/js/postAdForm.js
+++ app/views/components/postAdForm/js/postAdForm.js
@@ -1,7 +1,10 @@
 'use strict';
 
 let initialize = () => {
-
+	// update title input char count
+	$('.title-input').on('keyup', (event) => {
+		$('.char-count').text(event.target.value.length);
+	});
 };
 
 module.exports = {
