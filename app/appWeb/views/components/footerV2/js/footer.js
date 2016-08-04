1f1cd4a9e613f04d6cc5445be615066776f3b566
Labana <nlabana@ebay.com>
2016-08-04 15:05:49 -0700

    fixed BOLT-22408

--- app/appWeb/views/components/footerV2/js/footer.js
+++ app/appWeb/views/components/footerV2/js/footer.js
@@ -5,13 +5,27 @@ let $ = require('jquery');
 let initialize = () => {
 
 	let _toggleCategory = (e) => {
+
 		$(e.currentTarget).find('.menu-items').toggleClass('hide');
+		$(e.currentTarget).parents('.footer-inner-wrappper').toggleClass('expanded');
+
+		if ($('.footer-inner-wrappper:nth-child(3n)').hasClass('expanded')) {
+			$('.seo-links').css({'display': 'inline-block', 'top': '-15em'});
+
+		} else {
+			$('.seo-links').css('display', 'none');
+		}
+
 	};
 
 	$(document).ready(() => {
+		$('.menu-items').addClass('hide');
+		$('.footer-inner-wrappper').removeClass('inner');
 		$('.menu-category').on('click', (e) => {
 			_toggleCategory(e);
+
 		});
+
 	});
 };
 
