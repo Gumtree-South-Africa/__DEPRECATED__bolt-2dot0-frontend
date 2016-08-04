c25e4aa5c98f834a43d3d280059cd510b4e74d8d
Mike Soennichsen <msoennichsen@ebay.com>
2016-08-04 16:42:34 -0700

    More client side tests, fixed css styles, removed Q from clientside javascript

--- test/clientUnit/spec/postAdSpec.js
+++ test/clientUnit/spec/postAdSpec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let uploadImageController = require("app/appWeb/views/components/uploadImage/js/uploadImage.js");
+let uploadAdController = require("app/appWeb/views/components/uploadImage/js/uploadAd.js");
 let ImageHelper = require('app/appWeb/views/components/uploadImage/js/imageHelper.js');
 let specHelper = require('../helpers/commonSpecHelper.js');
 
@@ -9,29 +10,62 @@ let imageHelper = new ImageHelper({
 	IsEbayDirectUL: true
 });
 
+let mockPostAdResponse = {
+	"id": "1001100557900910658758009",
+	"_links": [
+		{
+			"rel": "self",
+			"href": "/ads/1001100557900910658758009",
+			"method": "GET"
+		},
+		{
+			"rel": "vipSeoUrl",
+			"href": "/a-venta-inmuebles/2-de-octubre/post-house-ad-from-bapi-at-2016+07+22-00-57-24-085/1001100557900910658758009",
+			"method": "GET"
+		}
+	]
+};
+
+
 fdescribe('Post Ad', () => {
 	describe('Upload Image (mobile)', () => {
-		it('Should update background image after EPS', () => {
-			let $testArea = specHelper.setupTest("uploadImage_es_MX", {
+		let $testArea;
+		let file;
+		beforeEach(() => {
+			$testArea = specHelper.setupTest("uploadImage_es_MX", {
 				eps: {
 					epsUploadExternalURL: '/eps',
 					isEbayDirectUploadEnabled: true
 				}
 			}, "es_MX");
 			specHelper.registerMockAjax('/eps', mockEpsResponse);
-
-			uploadImageController.initialize();
-			uploadImageController.loadData(0, {
+			specHelper.registerMockAjax('/api/postad/create', mockPostAdResponse);
+			file = {
 				size: 1024,
 				name: 'asdf.png'
-			});
+			};
+		});
 
-			let $postForm = $testArea.find('#mobileFileUpload');
-			let event = jQuery.Event('change');
-			$postForm.trigger(event);
+		it('Should update background image after EPS', () => {
+			uploadImageController.initialize();
+			uploadImageController.loadData(0, file);
+
 			let $imagePreview = $testArea.find('.user-image');
 			let imageUrl = imageHelper.convertThumbImgURL18(imageHelper.getThumbImgURL(mockEpsResponse));
 			expect($imagePreview.css('background-image')).toBe(`url("${imageUrl}")`);
+		});
+
+		it('Should not request location if cookie is set', () => {
+			let postAd = uploadAdController.postAd;
+			spyOn(uploadAdController, 'postAd').and.callFake((images, success, fail, options) => {
+				postAd(images, (response) => {
+					expect(response).toBe(mockPostAdResponse);
+					success(response);
+				}, fail, options);
+			});
+			document.cookie = 'geoId=123ng456';
+			uploadImageController.initialize();
+			uploadImageController.loadData(0, file);
 		});
 	});
 });
