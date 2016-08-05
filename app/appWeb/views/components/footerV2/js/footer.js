ef634c2aceaf0b5d4c6634f33b4a7e8c030e6fcd
Labana <nlabana@ebay.com>
2016-08-04 22:38:18 -0700

    Seo footer BOLT-22408

--- app/appWeb/views/components/footerV2/js/footer.js
+++ app/appWeb/views/components/footerV2/js/footer.js
@@ -9,11 +9,14 @@ let initialize = () => {
 		$(e.currentTarget).find('.menu-items').toggleClass('hide');
 		$(e.currentTarget).parents('.footer-inner-wrappper').toggleClass('expanded');
 
-		if ($('.footer-inner-wrappper:nth-child(3n)').hasClass('expanded')) {
-			$('.seo-links').css({'display': 'inline-block', 'top': '-15em'});
-
+		if ($('.footer-links div:nth-child(5)').hasClass('expanded')) {
+			 if ($('.footer-links div:nth-child(7)').hasClass('expanded')) {
+				 $('.seo-links').css({'display': 'inline-block', 'top': '-20.3em'});
+			 } else {
+				 $('.seo-links').css({'display': 'inline-block', 'top': '-15em'});
+			 }
 		} else {
-			$('.seo-links').css('display', 'none');
+           $('.seo-links').css('display', 'none');
 		}
 
 	};
