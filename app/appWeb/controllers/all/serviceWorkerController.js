6c92c41aad75829c8db0f482a109c0685d2ee251
Gao, George(AWF) <ggao1@ebay.com>
2016-08-07 21:45:45 -0700

    address eslint

--- app/appWeb/controllers/all/serviceWorkerController.js
+++ app/appWeb/controllers/all/serviceWorkerController.js
@@ -2,42 +2,45 @@
 
 let express = require('express');
 let router = express.Router();
+let config = require('config');
 let path = require('path');
+let cwd = process.cwd();
 let fs = require('fs');
 
-if (process.env.NODE_ENV !== undefined) {
-	// let cwd = process.cwd();
-	// let env = process.env.NODE_ENV || 'dev';
-
-	// let envHost = require(cwd + '/server/config/' + env + '.json').static.server.host;
-	// let envVersion = require(cwd + '/server/config/' + env + '.json').static.server.version;
-	// let baseJSUrl = require(cwd + '/server/config/' + env + '.json').static.baseJSUrl;
-}
-
-
+let ServiceWorkerModel = require(cwd + '/app/builders/page/ServiceWorkerModel');
 
 /**
  * Build Service worker registration
  */
-router.get('/', (req, res) => {
-
-	console.time('Instrument-ServiceWorker-Controller');
-
+router.get('/', (req, res, next) => {
 	let locale = res.locals.config.locale;
+	let serviceWorker = new ServiceWorkerModel(req, res);
+	let modelPromise = serviceWorker.populateData();
 
-	fs.readFile(process.cwd() + '/app/appWeb/serviceWorkers/service-worker-' + locale + '.js', 'utf8', function(err, data) {
-		if (err) {
-			return err;
-		} else {
-			return data;
-		}
+	modelPromise.then((modelData) => {
+		let urlHost = config.get('static.server.host');
+		let baseJSDir = (urlHost!==null) ? '' : '/public';
+		let cacheObj = 'let cacheObj =' + JSON.stringify({'homepagePreCache': modelData.homepagePreCache, 'homepageCache': modelData.homepageCache}) + ';';
+		let cacheFilePath = process.cwd() + baseJSDir + '/js/swCacheConfig.js';
+		fs.access(cacheFilePath, fs.F_OK, function(err) {
+			if (err) {
+				fs.writeFile(cacheFilePath, cacheObj, function(writeErr) {
+					if(err) {
+						return writeErr;
+					}
+					res.set('Content-Type', 'application/javascript');
+					res.sendFile(path.join(process.cwd() + '/app/appWeb/serviceWorkers/service-worker-' + locale + '.js'));
+				});
+			} else {
+				res.set('Content-Type', 'application/javascript');
+				res.sendFile(path.join(process.cwd() + '/app/appWeb/serviceWorkers/service-worker-' + locale + '.js'));
+			}
+		});
+	}).fail((err) => {
+		console.error(err);
+		console.error(err.stack);
+		next(err);
 	});
-
-	res.set('Content-Type', 'application/javascript');
-
-	res.sendFile(path.join(process.cwd() + '/app/appWeb/serviceWorkers/service-worker-' + locale + '.js'));
-
-	console.timeEnd('Instrument-ServiceWorker-Controller');
 });
 
 module.exports = router;
