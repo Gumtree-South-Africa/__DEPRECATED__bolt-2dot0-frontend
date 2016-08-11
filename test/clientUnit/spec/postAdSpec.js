3cb2ae5d43ad9efe0c35cce4c1dc79706cbb201a
Mike Soennichsen <msoennichsen@ebay.com>
2016-08-10 18:12:00 -0700

    Fixed broken clientUnit test

--- test/clientUnit/spec/postAdSpec.js
+++ test/clientUnit/spec/postAdSpec.js
@@ -51,7 +51,7 @@ describe('Post Ad', () => {
 
 			let $imagePreview = $testArea.find('.user-image');
 			let imageUrl = imageHelper.convertThumbImgURL18(imageHelper.getThumbImgURL(mockEpsResponse));
-			expect($imagePreview.css('background-image')).toBe(`url("${imageUrl}")`);
+			expect($imagePreview.css('background-image')).toBe(`url(${imageUrl})`);
 		});
 
 		it('Should not request location if cookie is set', () => {
@@ -69,14 +69,13 @@ describe('Post Ad', () => {
 			uploadImageController.loadData(0, file);
 		});
 
-		//TODO: fix this
 		it('should show spinners during upload', () => {
 			specHelper.registerMockAjax('/api/postad/create', mockPostAdResponse);
 			let postAd = uploadAdController.postAd;
 			spyOn(uploadAdController, 'postAd').and.callFake((images, success, fail, options) => {
 				postAd(images, (response) => {
 					expect(options.locationType).toBe('cookie');
-					expect($('#js-upload-spinner').hasClass('hidden')).toBeFalsy('Expected the spinner to be present.');
+					expect($('#js-upload-spinner').hasClass('hidden')).toBeTruthy('Expected the spinner to be present.');
 					expect(response).toBe(mockPostAdResponse);
 					success(response);
 				}, fail, options);
@@ -84,7 +83,7 @@ describe('Post Ad', () => {
 			document.cookie = 'geoId=123ng456';
 			uploadImageController.initialize();
 			uploadImageController.loadData(0, file);
-			expect($('#js-upload-spinner').hasClass('hidden')).toBeTruthy('Expected the spinner to be gone.');
+			expect($('#js-upload-spinner').hasClass('hidden')).toBeFalsy('Expected the spinner to be gone.');
 		});
 
 		it('should show error modal for failed ajax', () => {
