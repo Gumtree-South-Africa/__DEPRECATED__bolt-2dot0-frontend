1d8856943ac5682b828f1eec4e0396c9b13e50f5
Mike Soennichsen <msoennichsen@ebay.com>
2016-08-10 17:34:08 -0700

    Fix for hamburger menu on edit page, fixed carousel for mobile breakpoint

--- app/appWeb/views/templates/pages/editAd/js/editPage.js
+++ app/appWeb/views/templates/pages/editAd/js/editPage.js
@@ -23,6 +23,16 @@ let initialize = () => {
 		slickOptions: {
 			arrows: true,
 			infinite: false,
+			responsive: [
+				{
+					//TODO: update this breakpoint
+					breakpoint: 848,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				}
+			],
 			slidesToShow: 4,
 			slidesToScroll: 4
 		},
