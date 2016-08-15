fab84029aa996937e15514831a5140f3800f503d
Videep Raj <vrajendiran@ebay.com>
2016-08-15 11:35:54 -0700

    add updates to service worker file

--- app/appWeb/serviceWorkers/service-worker-es_MX.js
+++ app/appWeb/serviceWorkers/service-worker-es_MX.js
@@ -1,13 +1,22 @@
 'use strict';
 
-//Initial precaching homepage
+
+/**
+ * PRE-CACHE
+ **/
+// Precaching homepage
 toolbox.precache(['/']);
 
-//Adding set array to precache
+// Precaching homepage assets
 if (cacheObj) {
 	toolbox.precache(cacheObj['homepagePreCache']);
 }
 
+
+/**
+ * CACHE
+ */
+// Add homepage
 toolbox.router.get('/', toolbox.networkFirst, {
 	cache: {
 		name: 'vivanuncios-homepage',
@@ -16,7 +25,7 @@ toolbox.router.get('/', toolbox.networkFirst, {
 	}
 });
 
-//Adding set array to cache
+// Adding homepage assets
 if (cacheObj) {
 	 for (let cacheIndex = 0; cacheIndex < cacheObj['homepageCache'].length; cacheIndex++) {
 	 	toolbox.router.get(cacheObj.homepageCache[cacheIndex], toolbox.networkFirst, {
@@ -30,4 +39,56 @@ if (cacheObj) {
 }
 
 
+/**
+ * Offline Google Analytics
+ */
+var DB_NAME = 'offline-analytics';
+var EXPIRATION_TIME_DELTA = 86400000;
+var ORIGIN = /https?:\/\/((www|ssl)\.)?google-analytics\.com/;
 
+function replayQueuedAnalyticsRequests() {
+	simpleDB.open(DB_NAME).then(function(db) {
+		db.forEach(function(url, originalTimestamp) {
+			var timeDelta = Date.now() - originalTimestamp;
+			var replayUrl = url + '&qt=' + timeDelta;
+			fetch(replayUrl).then(function(response) {
+				if (response.status >= 500) {
+					return Response.error();
+				}
+				db.delete(url);
+			}).catch(function(error) {
+				if (timeDelta > EXPIRATION_TIME_DELTA) {
+					db.delete(url);
+				}
+			});
+		});
+	});
+}
+
+function queueFailedAnalyticsRequest(request) {
+	simpleDB.open(DB_NAME).then(function(db) {
+		db.set(request.url, Date.now());
+	});
+}
+
+function handleAnalyticsCollectionRequest(request) {
+	return global.fetch(request).then(function(response) {
+		if (response.status >= 500) {
+			return Response.error();
+		}
+		return response;
+	}).catch(function() {
+		queueFailedAnalyticsRequest(request);
+	});
+}
+
+toolbox.router.get('/collect',
+	handleAnalyticsCollectionRequest,
+	{origin: ORIGIN}
+);
+toolbox.router.get('/analytics.js',
+	toolbox.networkFirst,
+	{origin: ORIGIN}
+);
+
+replayQueuedAnalyticsRequests();
