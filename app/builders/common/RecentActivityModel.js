58fab6ce7f7db0827b1777b1c6f11ef0050dc053
Gao, George(AWF) <ggao1@ebay.com>
2016-08-18 13:31:57 -0700

    refactor recentActivities

--- app/builders/common/RecentActivityModel.js
+++ app/builders/common/RecentActivityModel.js
@@ -81,7 +81,7 @@ class RecentActivityModel {
 				feed3.prefix2 = feed3.attributes[1] ? feed3.attributes[1].prefix : '';
 				bapiResult.recent.push(feed3);
 			}
-			return this.transformData(bapiResult);
+			return bapiResult;
 		}).fail((bapiErr) => {
 			console.warn(`Error getting BAPI recentActivities data ${bapiErr}`);
 			return recentActivityService.getCachedRecentActivities(this.bapiHeaderValues).then((cachedResult) => {
@@ -96,32 +96,6 @@ class RecentActivityModel {
 				return {};
 			});
 		});
-	}
-
-	transformData(data) {
-		data.recent = [];
-
-		if (data.ads instanceof Array && data.ads.length>0) {
-			let feed1 = data.ads[Math.floor(Math.random() * data.ads.length)];
-			feed1.renderSold = this.isSold(feed1);
-			feed1.prefix1 = '11';
-			feed1.prefix2 = '12';
-			data.recent.push(feed1);
-
-			let feed2 = data.ads[Math.floor(Math.random() * data.ads.length)];
-			feed2.renderSold = this.isSold(feed2);
-			feed2.prefix1 = '21';
-			feed2.prefix2 = '22';
-			data.recent.push(feed2);
-
-			let feed3 = data.ads[Math.floor(Math.random() * data.ads.length)];
-			feed3.renderSold = this.isSold(feed3);
-			feed3.prefix1 = '31';
-			feed3.prefix2 = '32';
-			data.recent.push(feed3);
-		}
-
-		return data;
 	}
 }
 
