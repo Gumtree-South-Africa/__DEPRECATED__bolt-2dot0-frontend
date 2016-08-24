20760c4bbc41f935d4fc68876e541239e0a2abb1
Mike Soennichsen <msoennichsen@ebay.com>
2016-08-24 10:12:33 -0700

    Added serverUnit test to validate fields were being populated from ad data

--- app/appWeb/controllers/api/apiPostAdController.js
+++ app/appWeb/controllers/api/apiPostAdController.js
@@ -151,6 +151,7 @@ router.post('/create', cors, (req, res) => {
 		}).fail((error) => {
 			// post ad has failed
 			console.error(`postAdModel.postAd failure ${error}`);
+			console.error(error.data);
 			res.status(500).send({
 				error: "postAd failed, see logs for details"
 			});
--- test/serverUnit/spec/editAdSpec.js
+++ test/serverUnit/spec/editAdSpec.js
@@ -2,7 +2,7 @@
 let specHelper = require('../helpers/specHelper');
 let boltSupertest = specHelper.boltSupertest;
 let endpoints = require(`${process.cwd()}/server/config/mock.json`).BAPI.endpoints;
-// let cheerio = require('cheerio');
+let cheerio = require('cheerio');
 
 describe('Edit Ad Page', () => {
 	// let i18n;
@@ -49,6 +49,39 @@ describe('Edit Ad Page', () => {
 			supertest
 				.expect((res) => {
 					expect(res.status).toBe(404);
+				})
+				.end(specHelper.finish(done));
+		});
+	});
+
+	it('should populate the fields with correct data from BAPI', (done) => {
+		specHelper.registerMockEndpoint(
+			endpoints.specificAd.replace('{id}', '12'),
+			'test/serverUnit/mockData/api/v1/GetAdResponse.json');
+		specHelper.registerMockEndpoint(
+			endpoints.categoryAttributes.replace('{catId}', '9000'),
+			'test/serverUnit/mockData/api/v1/GetAttributeDefinitionsResponse.json'
+		);
+		let mockData = require('../mockData/api/v1/GetAdResponse.json');
+
+		boltSupertest('/edit/12').then((supertest) => {
+			supertest
+				.expect((res) => {
+					expect(res.status).toBe(200);
+
+					let c$ = cheerio.load(res.text);
+					expect(c$(`input[title='adTitle']`).val()).toBe(mockData.title);
+					expect(c$(`#description-input`).text()).toBe(mockData.description);
+					expect(c$(`input[title='adPrice']`).val()).toBe(`${mockData.price.amount}`);
+					expect(c$(`input[value='USD']`).is(':checked')).toBeTruthy();
+
+					expect(c$(`input[name='AlmVehicleBrand']`).val()).toBe(mockData.attributes[0].value.attributeValue);
+					expect(c$(`input[name='AlmVehicleModel']`).val()).toBe(mockData.attributes[2].value.attributeValue);
+
+					let imageJson = JSON.parse(c$('#image-urls').text()).sizeUrls;
+					expect(imageJson.length).toBe(2);
+					expect(imageJson[0].LARGE).toBe(mockData.pictures.sizeUrls[0].LARGE);
+					expect(imageJson[1].SMALL).toBe(mockData.pictures.sizeUrls[1].SMALL);
 				})
 				.end(specHelper.finish(done));
 		});
