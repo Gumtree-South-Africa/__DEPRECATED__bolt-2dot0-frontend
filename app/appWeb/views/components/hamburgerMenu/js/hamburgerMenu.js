3a5b4014d6da84b83363616e9356273db2c92e56
Gary Zhang <garyzhang@ebay.com>
2016-08-24 09:51:50 -0700

    Pulling searchbar into header and restyling

--- app/appWeb/views/components/hamburgerMenu/js/hamburgerMenu.js
+++ app/appWeb/views/components/hamburgerMenu/js/hamburgerMenu.js
@@ -23,6 +23,9 @@ let toggleMenu = () => {
 	this.$header.animate({
 		left: (this.open) ? 0 : '70%'
 	});
+	this.$searchbar.animate({
+		left: (this.open) ? 0 : '70%'
+	});
 	this.$pageContent.toggleClass('menu-closed');
 	this.$hamburgerContents.toggleClass('hamburger-open hamburger-closed');
 	this.open = !this.open;
@@ -55,6 +58,7 @@ let initialize = () => {
 	this.$browseCategories = $('#js-browse-categories', this.$browse);
 	this.$modalFooter = $('.modal-footer');
 	this.$header = $('.headerV2');
+	this.$searchbar = $("#search-bar");
 	this.$pageContent = $('.containment');
 	this.$hamburgerPopout = $('#js-body-overlay');
 	this.$hamburgerIcon = $('#js-hamburger-icon');
