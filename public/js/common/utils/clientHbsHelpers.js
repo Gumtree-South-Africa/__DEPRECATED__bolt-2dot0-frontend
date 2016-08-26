f019ecbf9041c1b5ef17259d9994aa64e6c64d92
Kevin Bek <kbek@ebay.com>
2016-08-26 11:02:46 -0700

    Missing Helpers

--- public/js/common/utils/clientHbsHelpers.js
+++ public/js/common/utils/clientHbsHelpers.js
@@ -92,6 +92,10 @@ let initialize = (Handlebars) => {
 		let str = keyvalue.split(":");
 		return new Handlebars.SafeString(str[1]);
 	});
+
+	Object.keys(comparisonHelpers).forEach((helperName) => {
+		Handlebars.registerHelper(helperName, comparisonHelpers[helperName]);
+	});
 };
 
 module.exports = {
