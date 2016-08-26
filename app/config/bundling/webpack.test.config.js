908b93fb981e890ab4df5e974748ee3f7c7b2934
Dennis ONeill <denoneill@ebay.com>
2016-08-26 15:29:26 -0700

    Fixing Client Unit Webpack Loaders

--- app/config/bundling/webpack.test.config.js
+++ app/config/bundling/webpack.test.config.js
@@ -6,17 +6,13 @@ let webpack = require('webpack');
 let baseWebpackConfig = require("./webpack.base.config.js");
 
 let testWebpackConfig = {
-	devtool: "inline-source-map",
-	module: {
-		// for client unit tests, allow json files to be required
-		loaders: [
-			{
-				test: /\.json$/,
-				loader: "json-loader"
-			}
-		]
-	}
+	devtool: "inline-source-map"
 };
+
+baseWebpackConfig.module.loaders.push({
+	test: /\.json$/,
+	loader: "json-loader"
+});
 
 baseWebpackConfig.plugins.shift(); //Dont UGlify tests
 baseWebpackConfig.plugins.shift(); // Dont common chunk creation
