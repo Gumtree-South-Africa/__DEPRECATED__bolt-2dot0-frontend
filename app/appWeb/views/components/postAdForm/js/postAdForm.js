e94e753ce73f1a727db55ac54c4d81c8922c6d0b
Mike Soennichsen <msoennichsen@ebay.com>
2016-08-30 16:54:18 -0700

    Fix for BOLT-23245

--- app/appWeb/views/components/postAdForm/js/postAdForm.js
+++ app/appWeb/views/components/postAdForm/js/postAdForm.js
@@ -1,10 +1,30 @@
 'use strict';
 
+let _bindEvents = () => {
+	this.$priceInput.on('keydown', (e) => {
+		if (e.keyCode > 57 || e.keyCode < 48) {
+			switch (e.keyCode) {
+				case 8://backspace
+				case 9://tab
+				case 13://delete
+				case 37://left arrow
+				case 39://right arrow
+					break;
+				default:
+					e.preventDefault();
+					break;
+			}
+		}
+	});
+};
+
 let initialize = () => {
 	// update title input char count
 	$('.title-input').on('keyup', (event) => {
 		$('.char-count').text(event.target.value.length);
 	});
+	this.$priceInput = $('#price-input');
+	_bindEvents();
 };
 
 module.exports = {
