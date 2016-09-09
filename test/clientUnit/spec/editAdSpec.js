6758b7c82b985f1dab7cc23144ddb68cf48ab89e
Kevin Bek <kbek@ebay.com>
2016-09-09 14:00:51 -0700

    Test Commit

--- test/clientUnit/spec/editAdSpec.js
+++ test/clientUnit/spec/editAdSpec.js
@@ -9,7 +9,7 @@ let mockLatLongData = {"id":201,"localizedName":"Mexico City","level":"L0","isLe
 
 let mockCategoryTree = require("../mock/categoryTree.json");
 
-describe('Edit Ad', () => {
+xdescribe('Edit Ad', () => {
 	it("should make ajax call when button is clicked", () => {
 		let $testArea = specHelper.setupTest("editAdFormMainDetails_es_MX", {
 			categoryCurrentHierarchy: "[0, 3]",
