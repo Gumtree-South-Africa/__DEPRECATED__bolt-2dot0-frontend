f411d7a67930532febb46e8071c443669c003b3e
Mike Soennichsen <msoennichsen@ebay.com>
2016-09-14 15:35:10 -0700

    BOLT-23661, BOLT-23672

--- app/appWeb/views/components/loginModal/js/loginModal.js
+++ app/appWeb/views/components/loginModal/js/loginModal.js
@@ -1,6 +1,27 @@
 'use strict';
 
-let initialize = () => {};
+let _checkConsent = (e) => {
+	if (!this.hasConsented) {
+		this.$consentError.removeClass('hidden');
+		e.preventDefault();
+	}
+};
+
+let initialize = () => {
+	this.$loginModal = $('.login-modal');
+	this.$facebookButton = this.$loginModal.find('#facebook-link');
+	this.$consentCheck = this.$loginModal.find('#consent-checkbox');
+	this.$consentError = this.$loginModal.find('#consent-error');
+	this.hasConsented = this.$consentCheck.is(':checked');
+	this.$consentCheck.on('change', () => {
+		this.hasConsented = this.$consentCheck.is(':checked');
+		if (this.hasConsented && !this.$consentError.hasClass('hidden')) {
+			this.$consentError.addClass('hidden');
+		}
+	});
+
+	this.$facebookButton.on('click', _checkConsent);
+};
 
 module.exports = {
 	initialize
