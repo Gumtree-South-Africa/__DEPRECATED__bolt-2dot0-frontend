34b5a41edd186c2cbc75c744bbfc2bd623d26d2a
Gao, George(AWF) <ggao1@ebay.com>
2016-09-20 13:16:08 -0700

    firefox bug

--- app/appWeb/serviceWorkers/service-worker-es_MX.js
+++ app/appWeb/serviceWorkers/service-worker-es_MX.js
@@ -49,13 +49,13 @@ self.addEventListener('activate', function(event) {
 	event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
-});
+// self.addEventListener('fetch', function(event) {
+// 	event.respondWith(
+// 		caches.match(event.request).then(function(response) {
+// 			return response || fetch(event.request);
+// 		})
+// 	);
+// });
 
 
 /**
