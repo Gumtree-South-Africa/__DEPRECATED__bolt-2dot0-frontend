f61f3ea951892642deed9ccd62e03d2015358021
Videep Raj <vrajendiran@ebay.com>
2016-09-26 13:54:48 -0700

    fix ip address resolution

--- server/middlewares/check-ip.js
+++ server/middlewares/check-ip.js
@@ -1,11 +1,17 @@
 'use strict';
 
-let ipwareGetip = require('ipware')(process.cwd() + '/server/middlewares/check-ip-config.json').get_ip;
+let ipwareGetip = require('ipware')(process.cwd() + '/server/middlewares/check-ip-config.json');
 
 module.exports = function() {
 	return function(req, res, next) {
-		let result = ipwareGetip(req);
-		req.app.locals.ip = result.clientIp;
+		let result = ipwareGetip.get_ip(req, false);
+
+		let ip = result.clientIp;
+		if (ip.substr(0, 7) === '::ffff:') {
+			ip = ip.substr(7);
+		}
+
+		req.app.locals.ip = ip;
 
 		// call next middleware
 		next();
