ee4e38ab813f2a8f572479ccefa0fb396cc760d0
Huerta <ernhuerta@lm-sjc-11003748.corp.ebay.com>
2016-09-28 17:55:31 -0500

    fix(PostAdPageConfig.js) fixing the js issue on the postad

--- app/config/bundling/pages/PostAdPageConfig.js
+++ app/config/bundling/pages/PostAdPageConfig.js
@@ -39,6 +39,11 @@ module.exports = {
 			"core": {}, // locale specific core modules have the third h
 			"mobile": {}, // device and locale specific has the highest priority
 			"desktop": {}
+		},
+		"en_ZA": {
+			"core": {}, // locale specific core modules have the third h
+			"mobile": {}, // device and locale specific has the highest priority
+			"desktop": {}
 		}
 	}
 };
