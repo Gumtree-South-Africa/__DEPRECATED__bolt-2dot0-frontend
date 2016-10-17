dac03b202f825dbd93b2c105925e99a67a734d28
Videep Raj <vrajendiran@ebay.com>
2016-10-17 12:02:16 -0700

    fix clientUnit for chrome

--- test/clientUnit/karmaConfig/karma.chrome.conf.js
+++ test/clientUnit/karmaConfig/karma.chrome.conf.js
@@ -1,33 +1,41 @@
-"use strict";
-let webpackConfig = require(process.cwd() + "/app/config/bundling/webpack.test.config.js");
+'use strict';
+let webpackConfig = require(process.cwd() + '/app/config/bundling/webpack.test.config.js');
 module.exports = function (config) {
         config.set({
-                frameworks: ["jasmine"],
+                frameworks: ['jasmine'],
                 files: [
-                        "../../../public/js/libraries/handlebars/handlebars-v4.0.5.js",
-                        "../helpers/webTemplates.js",
-                        "../../../node_modules/jquery/dist/jquery.js",
-                        "../../../public/js/common/tracking/Analytics.js",
-                        "../SpecRunner.js"
+					'public/js/libraries/handlebars/handlebars-v4.0.5.js',
+					'test/clientUnit/helpers/webTemplates.js',
+					'node_modules/jquery/dist/jquery.js',
+					'public/js/common/tracking/Analytics.js',
+					'test/clientUnit/SpecRunner.js'
                 ],
                 browsers: ['Chrome_custom'],
-                reporters: ["spec"],
+                reporters: ['spec', 'coverage'],
                 preprocessors: {
-                        '../**/*.js': ['webpack']
+					'test/clientUnit/**/*.js': ['webpack']
                 },
+				coverageReporter: {
+					type : 'html',
+					dir : 'test/clientUnit/coverage/',
+					includeAllSources: true
+				},
                 // you can define custom flags
                 customLaunchers: {
-                        'Chrome_custom': {
-                                base: 'Chrome',
-                                flags: ['--no-sandbox'] // with sandbox it fails under Docker
-                        }
+					'Chrome_custom': {
+						base: 'Chrome',
+						flags: ['--no-sandbox'] // with sandbox it fails under Docker
+					}
                 },
                 webpack: webpackConfig,
                 plugins: [
-                        "karma-spec-reporter",
-                        "karma-jasmine",
-                        require("karma-webpack"),
-                        "karma-chrome-launcher"
+					'karma-sourcemap-loader',
+					'karma-spec-reporter',
+					require('karma-webpack'),
+					'karma-chrome-launcher',
+					'karma-coverage',
+					'istanbul-instrumenter-loader',
+					'karma-jasmine'
                 ]
         });
 };
