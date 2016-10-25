a0520bbf48ca563be129c28ba1622db0d43d31ee
Gao, George(AWF) <ggao1@ebay.com>
2016-10-25 11:24:40 -0700

    add unit tests to ad details

--- server/services/mockData/adDetailsMock.js
+++ server/services/mockData/adDetailsMock.js
@@ -1,37 +1,28 @@
 'use strict';
 
 module.exports = {
-	'adDetails': [
-		{
-			'title': 'Cozy 3 Bedroom Apartment',
-			'price': '$150',
-			'priAttributes': [
-				{
-					'bedroom': '3'
-				},
-				{
-					'size': '700 m'
-				},
-				{
-					'garage': '2'
-				},
-				{
-					'bathrooms': '2'
-				},
-				{
-					'pets': 'OK'
-				}
-			],
-			'description': 'Cozy 3 bedroom apartment in the heart of downtown Mexico, blah blah blah blah blah blah',
-			'phone': '808-555-5555',
-			'secAttributes': [
-				{'categoryT1': 'T1 Category'},
-				{'categoryT2': 'T2 Category'},
-				{'categoryT3': 'T3 Category'},
-				{'location': 'Mexico City'},
-				{'lastUpdated': '1 day ago'},
-				{'postedBy': 'Owner'}
-			]
-		}
-	]
+	data : {
+		'userId': 'testUser123',
+		'title': 'Cozy 3 Bedroom Apartment',
+		'price': '$150',
+		'posted': '3 days ago',
+		'priAttributes': {
+			'BEDROOM': '3',
+			'SIZE': '700 m',
+			'GARAGE': '2',
+			'BATHROOMS': '2',
+			'PETS': 'OK'
+		},
+		'description': 'Cozy 3 bedroom apartment in the heart of downtown Mexico City. A quick 15 minute walked to the lively downtown scene where you can find shopping, food and all night music and dancing. First Months rent and $1500 security deposit due upon signing lease.',
+		'phone': '808-555-5555',
+		'viewCount': '44',
+		'repliesCount': '3',
+		'category': 'T1 Category Goes Here / T2 Category Goes Here / T3 Category Goes Here',
+		'categoryT1': 'T1 Category',
+		'categoryT2': 'T2 Category',
+		'categoryT3': 'T3 Category',
+		'location': 'Mexico City',
+		'lastUpdated': '1 Day Ago',
+		'postedBy': 'Owner'
+	}
 };
