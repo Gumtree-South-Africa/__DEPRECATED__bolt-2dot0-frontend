781f4ebbaa96440e880a4808017e93f264c5c1f0
NACER GARGA <ngarga@ebay.com>
2016-10-26 22:29:40 -0500

    implementation of vip-carousel

--- app/appWeb/views/components/viewPageGallery/js/viewPageGallery.js
+++ app/appWeb/views/components/viewPageGallery/js/viewPageGallery.js
@@ -1,9 +1,69 @@
 'use strict';
 
-class ViewPageGallery {
-	initialize() {
+require("slick-carousel");
+
+class viewPageGallery {
+
+	/**
+	 * sets up all the variables and two functions (success and failure)
+	 * these functions are in here to be properly bound to this
+	 * @param options
+	 */
+	initialize(options) {
+		if (!options) {
+			options = {
+				slickOptions: {
+					arrows: false,
+					infinite: false,
+					slidesToShow: 1,
+					slidesToScroll: 1
+				},
+			};
+		}
+
+		this.$vipGallery = $('#vipGallery');
+
+		// Slick setup
+		this.$vipGallery.find('.vip-gallery').slick(options.slickOptions);
+
+		this.$vipGallery.find('.slick-arrow').addClass('icon-back');
+
+		this.$vipGallery.find('.vip-gallery').on('breakpoint', () => {
+			this.resizeCarousel();
+		});
+
+		this.$vipGallery.find('.slick-slide').on('click', (evt) => {
+			let cItems = document.querySelectorAll('.slick-carousel');
+			[].forEach.call(cItems, (item) => {
+				$(item).removeClass('selected');
+			});
+			$(evt.target).addClass('selected');
+			this.updateMainImage(evt);
+			this.updatePhotoCounter($(evt.target).context.attributes[2].value);
+		});
+
+		//update photo count
+		//this.updatePhotoCounter(0);
 
 	}
+
+	resizeCarousel() {
+		$('#vipGallery .vip-gallery').slick('unslick');
+		$('#vipGallery .vip-gallery').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		});
+	}
+
+	updateMainImage(event) {
+			let bgImg = $(event.target).context.style.backgroundImage;
+			this.$vipGallery.find('.main-bgImg').css('background-image', bgImg);
+	}
+
+	updatePhotoCounter(idx) {
+		this.$vipGallery.find('.counter .currentImg').html(++idx);
+	}
+
 }
 
-module.exports = new ViewPageGallery();
+module.exports = new viewPageGallery ();
--- app/appWeb/views/templates/pages/viewPage/js/viewPage.js
+++ app/appWeb/views/templates/pages/viewPage/js/viewPage.js
@@ -1,11 +1,34 @@
 'use strict';
 
-let viewContent = require("app/appWeb/views/components/registrationForm/js/registrationForm.js");
+let viewPageGallery = require('app/appWeb/views/components/viewPageGallery/js/viewPageGallery.js');
 
-class ViewPage {
-	initialize() {
-		viewContent.initialize();
-	}
-}
+//TODO: update this breakpoint
+const MEDIUM_BREAKPOINT = 848;
 
-module.exports = new ViewPage();
+let _slickOptions = {
+	arrows: true,
+	infinite: false,
+	responsive: [
+		{
+			breakpoint: MEDIUM_BREAKPOINT,
+			settings: {
+				arrows: false,
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	],
+	slidesToShow: 3,
+	slidesToScroll: 1
+};
+
+let initialize = () => {
+	viewPageGallery.initialize({
+		slickOptions: _slickOptions,
+		showImageTracking: true
+	});
+};
+
+module.exports = {
+	initialize
+};
