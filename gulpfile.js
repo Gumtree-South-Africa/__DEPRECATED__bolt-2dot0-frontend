f9b945e68587cce02eaf2434e6a696cdfddb3303
Spark Liu <spliu@ebay.com>
2016-11-04 15:11:24 +0800

    Clean up unused import

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,8 @@
 
 
 var gulp = require('gulp'),
-	concat = require('gulp-concat'),
-	del = require('del'),
-	plumber = require('gulp-plumber'),
-	livereload = require('gulp-livereload'),
-	handlebars = require('gulp-handlebars'),
-	notify = require('gulp-notify'),
-	declare = require('gulp-declare'),
-	copy = require('gulp-copy'),
-	env = require('gulp-env'),
-	browserSync = require('browser-sync'),
-	map = require('map-stream'),
 	plugins = require('gulp-load-plugins')(),
-	compass = require('gulp-compass'),
-	jasmineBrowser = require('gulp-jasmine-browser'),
-	clean = require('gulp-clean'),
-	tar = require('gulp-tar'),
-	bump = require('gulp-bump'),
-	rename = require('gulp-rename'),
-	through2 = require('through2'),
-	path = require('path'),
-	mkdirp = require('mkdirp'),
-	newer = require('gulp-newer'),
-	phantom = require('phantom'),
-	cssmin = require('gulp-cssmin'),
-	fs = require('fs'),
-	runSequence = require('gulp-run-sequence'),
-	reload = browserSync.reload;
-
-
-// ////////////////////////////////////////////////
-// Log Errors
-// // /////////////////////////////////////////////
-function errorlog(err) {
-	console.error(err.message);
-	this.emit('end');
-}
+	runSequence = require('gulp-run-sequence');
 
 // ////////////////////////////////////////////////
 // Register gulp tasks
