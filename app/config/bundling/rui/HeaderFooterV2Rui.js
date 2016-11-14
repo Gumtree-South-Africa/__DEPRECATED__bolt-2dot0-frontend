d49ea4a4d543bb0566508fdddf028a9885382b38
NACER GARGA <ngarga@ebay.com>
2016-11-13 17:35:57 -0800

    reverting

--- app/config/bundling/rui/HeaderFooterV2Rui.js
+++ app/config/bundling/rui/HeaderFooterV2Rui.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('./../../../../public/js/libraries/jQuery/jquery-2.0.0.js');
 require('./../../../appWeb/views/components/headerV2/js/header.js').initialize();
 require('./../../../appWeb/views/components/profileMenu/js/profileMenu.js').initialize();
 require('./../../../appWeb/views/components/footerV2/js/footer.js').initialize();
