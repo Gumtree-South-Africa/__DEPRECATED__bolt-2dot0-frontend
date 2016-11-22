f7510343c953ef0cf73591e9185453c8f1bf9a60
Videep Raj <vrajendiran@ebay.com>
2016-11-22 15:57:03 -0800

    integrate sellerDetails on VIP, remove viewPageDetails, move adStats to component

--- app/config/bundling/pages/ViewPageConfig.js
+++ app/config/bundling/pages/ViewPageConfig.js
@@ -18,7 +18,6 @@ module.exports = {
 			"Footer": "app/appWeb/views/components/footerV2/js/footer.js",
 			"PageMain": "app/appWeb/views/templates/pages/viewPage/js/viewPage.js",
 			"AdDetails": "app/appWeb/views/components/adDetails/js/adDetails.js",
-		//	"ViewPageDetails": "app/appWeb/views/components/viewPageDetails/js/viewPageDetails.js",
 			"ViewPageGallery": "app/appWeb/views/components/viewPageGallery/js/viewPageGallery.js"
 		},
 		"mobile": {},
