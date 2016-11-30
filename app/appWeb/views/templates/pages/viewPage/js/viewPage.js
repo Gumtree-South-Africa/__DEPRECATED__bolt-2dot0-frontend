87e91c755f934b165003b66b6fccf42123d4d487
NACER GARGA <ngarga@ebay.com>
2016-11-29 19:05:02 -0800

    fix for images less than 5

--- app/appWeb/views/templates/pages/viewPage/js/viewPage.js
+++ app/appWeb/views/templates/pages/viewPage/js/viewPage.js
@@ -2,33 +2,43 @@
 
 let viewPageGallery = require('app/appWeb/views/components/viewPageGallery/js/viewPageGallery.js');
 
-//TODO: update this breakpoint
-const MEDIUM_BREAKPOINT = 848;
+// BEGIN: fix for 2, 3, 4 and 5 images
+let constImgLength = parseInt($('.container .counter').attr('data-image-length'));
 
-let _slickOptions = {
-	arrows: true,
-	infinite: true,
-	dots: true,
-	responsive: [
-		{
-			breakpoint: MEDIUM_BREAKPOINT,
-			settings: {
-				arrows: false,
-				infinite: true,
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}
-	],
-	slidesToShow: 5,
-	slidesToScroll: 1
-};
+if (constImgLength > 5) {
+	constImgLength = 5;
+} else if (constImgLength >= 2) {
+	let $cloneImg = $('.container .slider-nav').clone().html();
+	let $cloneImgOverlay = $('#vipOverlay .slider-nav').clone().html();
+	let $cloneImgFor = $('.container .slider-for').clone().html();
+	let $cloneImgOverlayFor = $('#vipOverlay .slider-for').clone().html();
+	$('.container .slider-nav').append($cloneImg);
+	$('#vipOverlay .slider-nav').append($cloneImgOverlay);
+	$('.container .slider-for').append($cloneImgFor);
+	$('#vipOverlay .slider-for').append($cloneImgOverlayFor);
+}
+
+switch (constImgLength) {
+	case 1:
+		$('.slider-nav').addClass('col1');
+		break;
+	case 2:
+		$('.slider-nav').addClass('col2');
+		break;
+	case 3:
+		$('.slider-nav').addClass('col3');
+		break;
+	case 4:
+			$('.slider-nav').addClass('col4');
+			break;
+	default:
+		break;
+}
+//END
+
 
 let initialize = () => {
-	viewPageGallery.initialize({
-		slickOptions: _slickOptions,
-		showImageTracking: true
-	});
+	viewPageGallery.initialize(constImgLength);
 };
 
 module.exports = {
