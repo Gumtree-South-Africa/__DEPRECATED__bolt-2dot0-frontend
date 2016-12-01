5ceef20d7832c632ef0f4a098ce42a13730f54d4
Pacheco, Ivan(AWF) <ivpacheco@ebay.com>
2016-12-01 11:09:43 -0600

    style(clientUnit/breadcrumbsSpec): fixes ESLint warnings

--- test/clientUnit/spec/breadcrumbsSpec.js
+++ test/clientUnit/spec/breadcrumbsSpec.js
@@ -1,17 +1,17 @@
-'use strict'
+'use strict';
 
-let model = require('../mockData/breadcrumbsModel.json')
-let specHelper = require('../helpers/commonSpecHelper.js')
+let model = require('../mockData/breadcrumbsModel.json');
+let specHelper = require('../helpers/commonSpecHelper.js');
 
 describe('Breadcrumbs', () => {
 	it('should find breadcrumbs', () => {
-		let $testArea = specHelper.setupTest("breadcrumbsV2_es_MX", { }, "es_MX");
+		specHelper.setupTest("breadcrumbsV2_es_MX", { }, "es_MX");
 
 		expect($('.breadcrumbs').length).toBe(1);
 	});
 
 	it('should have the proper number of elements', () => {
-		let $testArea = specHelper.setupTest("breadcrumbsV2_es_MX", model.locationsAndAd, "es_MX");
+		specHelper.setupTest("breadcrumbsV2_es_MX", model.locationsAndAd, "es_MX");
 
 		expect($('.breadcrumbs li').length).toBe(model.locationsAndAd.breadcrumbs.locations.length + 1);
 		expect($('.breadcrumb-location').length).toBe(model.locationsAndAd.breadcrumbs.locations.length);
@@ -19,7 +19,7 @@ describe('Breadcrumbs', () => {
 	});
 
 	it('should render all item properties', () => {
-		let $testArea = specHelper.setupTest("breadcrumbsV2_es_MX", model.locationsOnly, "es_MX");
+		specHelper.setupTest("breadcrumbsV2_es_MX", model.locationsOnly, "es_MX");
 
 		let $item = $('.breadcrumbs li').last();
 
@@ -27,6 +27,4 @@ describe('Breadcrumbs', () => {
 		expect($item.find('[itemprop=item]').attr('href')).toBeTruthy('should render the link URL by itself');
 		expect(parseInt($item.find('[itemprop=position]').attr('content'), 10)).toBeTruthy('should have an item position greater than or equal to 1');
 	});
-
-
-})
+});
