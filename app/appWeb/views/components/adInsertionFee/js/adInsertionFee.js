4edfe25beaa0ebe0d4cbfbb3affd3216fcaa3119
Charlie.Lang <chalang@ebay.com>
2016-12-07 16:03:06 +0800

    BOLT-24177, change L10n

--- app/appWeb/views/components/adInsertionFee/js/adInsertionFee.js
+++ app/appWeb/views/components/adInsertionFee/js/adInsertionFee.js
@@ -12,7 +12,7 @@ class AdInsertionFee {
 		$(this._$promoteWithInf.find(".ad-first-pic")).css("background-image", "url('" + adInfo.imageUrls[0] + "')");
 		$(this._$promoteWithInf.find(".inf-amount")).html(innsetionFee);
 		$(this._$promoteWithInf.find(".ad-title")).html(adInfo.title);
-		$(this._$promoteWithInf.find(".ad-price")).html(adInfo.price.amount);
+		$(this._$promoteWithInf.find(".ad-price")).html(adInfo.price.amount + " " + (adInfo.price.currency === "MXN" ? "" : adInfo.price.currency));
 		$(this._$promoteWithInf.find(".inf-category-name")).html(categorySelectedName);
 		$(this._$promoteWithInf.find(".inf-cancel-button")).prop("href", cancelLink);
 		this._$promoteWithInf.toggleClass("hidden", false);
