0cfc6df12fa6aa76d3a6ed466ebd279dfbb36fc8
Spark Liu <spliu@ebay.com>
2016-12-16 14:16:35 +0800

    Add support to post
    There are more things to consider:
    1. What if there is redirect parameter with localhost domain?
    2. What for links in returned page?
    3. How to solve redirect between http and https

--- server/middlewares/rui-proxy.js
+++ server/middlewares/rui-proxy.js
@@ -2,6 +2,26 @@
 
 let config = require('config');
 let proxy = require('http-proxy-middleware');
+let stream = require('stream');
+
+function prepareBodyString(req) {
+	if (!req.headers) {
+		return null;
+	}
+	let contentType = req.headers['content-type'];
+	let rawString = null;
+	if (contentType === 'application/x-www-form-urlencoded') {
+		let values = [];
+		Object.keys(req.body).forEach((key) => {
+			values.push(encodeURIComponent(key) + '=' + encodeURIComponent(req.body[key].toString()));
+		});
+		rawString = values.join('&');
+	} else if (contentType === 'application/json') {
+		rawString = JSON.stringify(req.body);
+	}
+
+	return rawString;
+}
 
 /**
  * A proxy to access RUI server, which will help developer to access BOLT 1.0 and 2.0 under same site.
@@ -34,13 +54,16 @@ module.exports = function(app) {
 	}
 
 	// By default, we will use HTTPS no matter what protocol for current site
-	let ruiPort = ruiProxyConfig.port || 443;
-	let defaultTarget = `https://${ruiProxyConfig.domainBase}:${ruiPort}`;
+	let defaultTarget = 'https://' + ruiProxyConfig.domainBase;
+	if (!isNaN(ruiProxyConfig.port)) {
+		defaultTarget += ':' + ruiProxyConfig.port;
+	}
 	let proxyMiddlewareConfig = {
 		target: defaultTarget,
 		changeOrigin: true,
 		xfwd: true,
 		autoRewrite: true,
+		cookieDomainRewrite: '',
 		// Don't verify SSL as it's develop mode
 		secure: false,
 		router: (req) => {
@@ -54,7 +77,13 @@ module.exports = function(app) {
 				return defaultTarget;
 			}
 			let ruiDomain = hostname.substring(0, hostnameIndex + configHostname.length);
-			return `https://${ruiDomain}.${ruiProxyConfig.domainBase}:${ruiPort}`;
+
+			let target = ((req.connection && req.connection.encrypted) ? 'https' : 'http') + '://' + ruiDomain + '.'
+				+ ruiProxyConfig.domainBase;
+			if (!isNaN(ruiProxyConfig.port)) {
+				target += ':' + ruiProxyConfig.port;
+			}
+			return target;
 		}
 	};
 	if (ruiProxyConfig.basePath) {
@@ -62,5 +91,36 @@ module.exports = function(app) {
 			'^/': ruiProxyConfig.basePath
 		};
 	}
-	return proxy(proxyMiddlewareConfig);
+
+	let proxyMiddleware = proxy(proxyMiddlewareConfig);
+
+	return (req, res, next) => {
+		let originalPipe = null;
+		let originalLength = null;
+		if (req.method === 'POST') {
+			// As stream of request has been consumed by body-parser, we need to reconstruct it
+			let bodyString = prepareBodyString(req);
+			if (bodyString) {
+				originalPipe = req.pipe;
+				originalLength = req.headers && req.headers['content-length'];
+
+				let bodyBuffer = new Buffer(bodyString, 'utf-8');
+				let bodyStream = new stream.PassThrough();
+				bodyStream.end(bodyBuffer);
+				req.pipe = (stream) => bodyStream.pipe(stream);
+				if (req.headers) {
+					req.headers['content-length'] = bodyBuffer.length;
+				}
+			}
+		}
+		proxyMiddleware(req, res, () => {
+			if (originalPipe) {
+				req.pipe = originalPipe;
+				if (req.headers) {
+					req.headers['content-length'] = originalLength;
+				}
+			}
+			next();
+		});
+	};
 };
