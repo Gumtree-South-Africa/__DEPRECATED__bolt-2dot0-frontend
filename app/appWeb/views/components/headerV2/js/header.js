8c38b034068de9c94be935c7e563a41937ab145f
mangino <hmangino@lm-sfa-11002394.corp.ebay.com>
2016-12-28 10:59:54 -0600

    feature(header.js header.css) new 2 level categoy menu in header

--- app/appWeb/views/components/headerV2/js/header.js
+++ app/appWeb/views/components/headerV2/js/header.js
@@ -4,6 +4,8 @@ let deepLink = require('app/appWeb/views/components/headerV2/js/deepLink.js');
 let hamburgerMenu = require("app/appWeb/views/components/hamburgerMenu/js/hamburgerMenu.js");
 require("app/appWeb/views/components/profileMenu/js/profileMenu.js").initialize();
 
+window._menuL2leaveOpen = false;
+
 let _toggleBrowseMenu = (shouldClose) => {
 	if (shouldClose === undefined) {
 		shouldClose = !this.$catDrop.hasClass('hidden');
@@ -12,6 +14,44 @@ let _toggleBrowseMenu = (shouldClose) => {
 	$('#js-browse-dropdown #js-browse-item-text').toggleClass('menu-open', !shouldClose);
 	this.$browseHeaderIcon.toggleClass('icon-down', shouldClose);
 	this.$browseHeaderIcon.toggleClass('icon-up', !shouldClose);
+};
+
+let showL1HideL2 = () => {
+
+	//$('#js-cat-dropdown').show();
+	$('.parent-l1').show();
+	$('.child-l2').addClass('hidden');
+	 window._menuL2leaveOpen = false;
+	 console.log('hghghghghg ' + window._menuL2leaveOpen);
+};
+
+let hideL1ShowL2 = (except) => {
+   $('.parent-l1').hide();
+   except.hide();
+   window._menuL2leaveOpen = true;
+   let children = '#' + except.attr('data-submenu');
+   $(children).removeClass('hidden');
+
+   let categoryTitle = $(except[0].innerHTML + ' span').text();
+   if ($('.subCatMenuHeader .subcat-title:eq(1)').length !== 0) {
+	   $(children + ' .subCatMenuHeader .subcat-title:eq(1)').text(categoryTitle);
+   }
+};
+
+let setupMenu = () => {
+	if ($('div#button-back').length !== 0) {
+		showL1HideL2();
+	}
+	$('.child-l2').addClass('hidden');
+	$('.parent-l1').click(function(e) {
+		e.preventDefault();
+	    hideL1ShowL2($(this));
+	   });
+
+   $('#js-cat-dropdown').on('click', '.back-button', showL1HideL2).on('click', '.link-item.l2', function() {
+	 _toggleBrowseMenu();
+	//  console.info('Ocultado');
+ });
 };
 
 let _toggleProfileMenu = (shouldClose) => {
@@ -27,12 +67,26 @@ let _toggleProfileMenu = (shouldClose) => {
 // onReady separated out for easy testing
 let onReady = () => {
 	this.$header.find('.browse').on('click', () => {
-		_toggleBrowseMenu();
+		//_toggleBrowseMenu();
 	}).mouseenter(() => {
 		_toggleBrowseMenu(false);
+		if ($('#js-cat-dropdown').mouseenter) {
+			console.log('mouseenter = true');
+		}
+
 	}).mouseleave(() => {
-		_toggleBrowseMenu(true);
+		if (!window._menuL2leaveOpen) {
+			_toggleBrowseMenu(true);
+		}
+
+		if ($('#js-cat-dropdown').mouseleave) {
+			console.log('mouseleave = true');
+		}
 	});
+	this.$header.find('.menu-item.parent-l1').on('click', () => {
+		_toggleBrowseMenu(false);
+	});
+
 
 	this.$header.find('.profile').on('click', () => {
 		_toggleProfileMenu();
@@ -66,7 +120,7 @@ class Header {
 
 // Note about registerOnReady - for tests only, call: .initialize(false) then invoke .onReady()
 let initialize = (registerOnReady = true) => {
-
+	setupMenu();
 	this.ishamburgerMenu = typeof($('#js-hamburger-menu')[0]) !== "undefined";
 
 	if (this.ishamburgerMenu) {
@@ -76,7 +130,6 @@ let initialize = (registerOnReady = true) => {
 	this.$header = $(".headerV2");
 	this.$profileDrop = this.$header.find('#js-profile-dropdown');
 	this.$profileHeaderIcon = this.$header.find("#js-profile-header-item-icon");
-
 	this.$catDrop = this.$header.find('#js-cat-dropdown');
 	this.$browseHeaderIcon = this.$header.find("#js-browse-header-item-icon");
 
